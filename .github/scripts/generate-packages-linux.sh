633b64b893359ac1c798a65d85ba08415ab82fd2
Łukasz Wroński <lukaszwronski98@gmail.com>
2021-06-17 20:12:08 +0200

    Fix windows CI - bash shell to run script

--- .github/scripts/generate-packages-linux.sh
+++ .github/scripts/generate-packages-linux.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 # Build scala-cli native image
 LAUNCHER="$(./mill -i show cli-core.nativeImage | jq -r | grep -oE "[^:]+$")"
 # Build deb package for scala-cli
-cs launch org.virtuslab::scala-packager-cli:0.1.7 -- --source-app-path "$LAUNCHER" --output scala-cli.deb --deb
-cs launch org.virtuslab::scala-packager-cli:0.1.7 -- --source-app-path "$LAUNCHER" --output scala-cli.rpm --rpm
+cs launch org.virtuslab::scala-packager-cli:0.1.10 -- --source-app-path "$LAUNCHER" --output scala-cli.deb --deb
+cs launch org.virtuslab::scala-packager-cli:0.1.10 -- --source-app-path "$LAUNCHER" --output scala-cli.rpm --rpm
 # Copy deb package to artifacts
 mkdir -p artifacts
 cp ./scala-cli.deb artifacts/scala-cli.deb
--- .github/scripts/generate-packages-win.sh
+++ .github/scripts/generate-packages-win.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 # Build scala-cli native image
-LAUNCHER="$(./mill -i show cli-core.standaloneLauncher | jq -r | grep -oE "[^:]+$")"
+LAUNCHER="$(./mill.bat -i show cli.standaloneLauncher)"
 # Build msi package for scala-cli
-cs launch org.virtuslab::scala-packager-cli:0.1.7 -- --source-app-path "$LAUNCHER" --output scala-cli.msi --msi
+cs launch org.virtuslab::scala-packager-cli:0.1.10 -- --source-app-path ./out/cli/standaloneLauncher/dest/launcher.bat --output scala-cli.msi --msi
 # Copy msi package to artifacts
 mkdir -p artifacts
-cp ./scala-cli.msi artifacts/scala-cli.msi
+ls artifacts
+mv ./scala-cli.msi artifacts/scala-cli.msi
+echo "artifacts/"
+ls artifacts
