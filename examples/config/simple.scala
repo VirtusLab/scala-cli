2d62b141d18d30c41b25de700c6ad8e01394c8d6
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-06-18 15:49:07 +0200

    Add config example

---
+++ examples/config/simple.scala
@@ -0,0 +1,14 @@
+import ai.kien.python._
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val javaVer = sys.props("java.version")
+    val scalaVer = scala.util.Properties.versionNumberString
+    println(s"Hello from Java $javaVer, Scala standard library $scalaVer")
+    val jnaLibPath = Python()
+      .scalaPyProperties
+      .getOrElse(Map.empty[String, String])
+      .getOrElse("jna.library.path", "")
+    println(s"You should set jna.library.path to $jnaLibPath")
+  }
+}
