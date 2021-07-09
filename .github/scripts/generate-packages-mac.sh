750c33860c645bf866206a1a3cb784752a1095d4
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-07-09 10:19:36 +0200

    Use main CLI launcher in macOS package
    cli-core is meant to be used when memory is tight on the CI, which is
    not the case on macOS runners.
    
    Plus the main cli launcher is supposed to be already built, unlike the
    cli-core one.

--- .github/scripts/generate-packages-mac.sh
+++ .github/scripts/generate-packages-mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 # Build scala-cli native image
-LAUNCHER="$(./mill -i show cli-core.nativeImage | tail -1 |  jq -r |  sed  -E 's/ref:[^:]*://')"
+LAUNCHER="$(./mill -i show cli.nativeImage | tail -1 |  jq -r |  sed  -E 's/ref:[^:]*://')"
 echo $LAUNCHER
 # Build pkg package for scala-cli
 cs launch org.virtuslab::scala-packager-cli:0.1.12 -- --source-app-path "$LAUNCHER" --output scala-cli.pkg --pkg
