1ae5a0740bb208b6680f03d3822dee4dc3601893
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-08-02 19:46:04 +0200

    Slightly safer ConfigMonoid derivation

--- modules/build/src/main/scala/scala/build/options/ConfigMonoid.scala
+++ modules/build/src/main/scala/scala/build/options/ConfigMonoid.scala
@@ -9,30 +9,39 @@ trait ConfigMonoid[T] {
 object ConfigMonoid {
   def apply[T](implicit instance: ConfigMonoid[T]): ConfigMonoid[T] = instance
 
-  implicit val hnil: ConfigMonoid[HNil] = {
-    (_, _) =>
-      HNil
+
+  trait HListConfigMonoid[T <: HList] {
+    def orElse(main: T, defaults: T): T
   }
 
-  implicit def hcons[H, T <: HList](implicit
-    headInstance: Lazy[ConfigMonoid[H]],
-    tailInstance: Lazy[ConfigMonoid[T]]
-  ): ConfigMonoid[H :: T] = {
-    (main, defaults) =>
-      val head = headInstance.value.orElse(main.head, defaults.head)
-      val tail = tailInstance.value.orElse(main.tail, defaults.tail)
-      head :: tail
+  object HListConfigMonoid {
+
+    implicit val hnil: HListConfigMonoid[HNil] = {
+      (_, _) =>
+        HNil
+    }
+
+    implicit def hcons[H, T <: HList](implicit
+      headInstance: ConfigMonoid[H],
+      tailInstance: Lazy[HListConfigMonoid[T]]
+    ): HListConfigMonoid[H :: T] = {
+      (main, defaults) =>
+        val head = headInstance.orElse(main.head, defaults.head)
+        val tail = tailInstance.value.orElse(main.tail, defaults.tail)
+        head :: tail
+    }
+
   }
 
   implicit def generic[T, R <: HList](implicit
     gen: Generic.Aux[T, R],
-    instance: Lazy[ConfigMonoid[R]]
+    instance: Lazy[HListConfigMonoid[R]]
   ): ConfigMonoid[T] =
     derive[T, R](gen, instance)
 
   def derive[T, R <: HList](implicit
     gen: Generic.Aux[T, R],
-    instance: Lazy[ConfigMonoid[R]]
+    instance: Lazy[HListConfigMonoid[R]]
   ): ConfigMonoid[T] = {
     (main, defaults) =>
       val r = instance.value.orElse(gen.to(main), gen.to(defaults))
