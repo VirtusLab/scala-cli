49a27a483f55b93ad7c4c6310f669b23ac6a4406
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-08-12 23:50:46 +0200

    Better message in package command

--- modules/build/src/main/scala/scala/build/options/PackageType.scala
+++ modules/build/src/main/scala/scala/build/options/PackageType.scala
@@ -1,15 +1,23 @@
 package scala.build.options
 
-sealed abstract class PackageType extends Product with Serializable
+sealed abstract class PackageType extends Product with Serializable {
+  def runnable: Boolean = false
+}
 
 object PackageType {
   sealed abstract class NativePackagerType extends PackageType
 
-  case object Bootstrap extends PackageType
+  case object Bootstrap extends PackageType {
+    override def runnable = true
+  }
   case object LibraryJar extends PackageType
-  case object Assembly extends PackageType
+  case object Assembly extends PackageType {
+    override def runnable = true
+  }
   case object Js extends PackageType
-  case object Native extends PackageType
+  case object Native extends PackageType {
+    override def runnable = true
+  }
   case object Debian extends NativePackagerType
   case object Dmg extends NativePackagerType
   case object Pkg extends NativePackagerType
