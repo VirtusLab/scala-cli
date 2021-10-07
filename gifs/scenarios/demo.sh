903f59010d5690233aae38a0fb4e8cf0c505cef8
Krzysztof Romanowski <romanowski.kr@gmail.com>
2021-10-07 23:24:19 +0200

    Add proper demo gif
    Add utilities to write and show scala files

--- gifs/scenarios/demo.sh
+++ gifs/scenarios/demo.sh
@@ -9,16 +9,46 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 if [[ -z "${ASCIINEMA_REC}" ]]; then
   # Warm up scala-cli
   echo "println(1)" | scala-cli -
+  echo "// using \"org.scalameta::munit:0.7.29\"" | scala-cli test -
   # or do other preparation (e.g. create code)
 else
   . $SCRIPT_DIR/../demo-magic.sh
   # # hide the evidence
   clear
 
-  # Put your stuff here
-  pe "echo 'println(\"TODO: turn gifs/scenarios/demo.sh into proper scenario general demo of Scala CLI\")' | scala-cli -"
+  updateFile demo.scala "@main def demo(args: String *) = println(args.mkStrink)"
+
+  pe "scala-cli compile demo.scala"
+
+  cat <<EOF | updateFile demo.scala
+def niceArgs(args: String*): String = 
+  args.map(_.capitalize).mkString("Hello: ", ", ", "!")
+
+@main def demo(args: String*) = println(niceArgs(args*))
+EOF
+
+  pe "scala-cli demo.scala -- Ala jake Mike"
+  
+  sleep 2
+
+  clear
+
+  cat <<EOF | updateFile demo.test.scala
+// using "org.scalameta::munit:0.7.29"
+
+class demoTest extends munit.FunSuite {
+  test("test nice args") {
+    assert(clue(niceArgs("a", "b")) == "Hello: A, B!")
+  }
+  test("test empty arguments") {
+    assert(clue(niceArgs()) == "Hello!")
+  }
+}
+EOF
+
+  pe "scala-cli test demo.scala demo.test.scala"
 
   # Wait a bit to read output of last command
-  sleep 2
+  sleep 5
   echo " "
 fi
