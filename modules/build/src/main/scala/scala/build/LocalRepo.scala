c31911e2a4be7d10b12da0f3d8f834805ca3f107
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-10-07 18:30:24 +0200

    Dummy refacto
    Useful for subsequent commits

--- modules/build/src/main/scala/scala/build/LocalRepo.scala
+++ modules/build/src/main/scala/scala/build/LocalRepo.scala
@@ -2,7 +2,7 @@ package scala.build
 
 import coursier.paths.Util
 
-import java.io.{BufferedInputStream, ByteArrayOutputStream, InputStream}
+import java.io.{BufferedInputStream, ByteArrayOutputStream, Closeable, InputStream}
 import java.nio.channels.{FileChannel, FileLock}
 import java.nio.file.{Path, StandardOpenOption}
 import java.util.zip.{ZipEntry, ZipInputStream}
@@ -14,6 +14,46 @@ object LocalRepo {
   private def resourcePath = Constants.localRepoResourcePath
   private def version      = Constants.localRepoVersion
 
+  private def using[S <: Closeable, T](is: => S)(f: S => T): T = {
+    var is0 = Option.empty[S]
+    try {
+      is0 = Some(is)
+      f(is0.get)
+    }
+    finally if (is0.nonEmpty) is0.get.close()
+  }
+
+  private def readContent(is: InputStream): Array[Byte] = {
+    val baos = new ByteArrayOutputStream
+
+    val buf  = Array.ofDim[Byte](16 * 1024)
+    var read = -1
+    while ({
+      read = is.read(buf)
+      read >= 0
+    })
+      baos.write(buf, 0, read)
+
+    baos.toByteArray
+  }
+
+  private def extractZip(zis: ZipInputStream, dest: os.Path): Unit = {
+    var ent: ZipEntry = null
+    while ({
+      ent = zis.getNextEntry()
+      ent != null
+    })
+      if (!ent.isDirectory) {
+        val content = readContent(zis)
+        zis.closeEntry()
+        os.write(
+          dest / ent.getName.split('/').toSeq,
+          content,
+          createFolders = true
+        )
+      }
+  }
+
   def localRepo(
     baseDir: os.Path,
     loader: ClassLoader = Thread.currentThread().getContextClassLoader
@@ -22,46 +62,21 @@ object LocalRepo {
 
     if (archiveUrl == null) None
     else {
-      val repoDir    = baseDir / version
-      val tmpRepoDir = repoDir / os.up / s".$version.tmp"
-      val repo       = "ivy:" + repoDir.toNIO.toUri.toASCIIString + "/[defaultPattern]"
+      val repoDir = baseDir / version
+
       if (!os.exists(repoDir))
         withLock((repoDir / os.up).toNIO, version) {
+          val tmpRepoDir = repoDir / os.up / s".$version.tmp"
           os.remove.all(tmpRepoDir)
-          var is: InputStream     = null
-          var zis: ZipInputStream = null
-          try {
-            is = archiveUrl.openStream()
-            zis = new ZipInputStream(new BufferedInputStream(is))
-            var ent: ZipEntry = null
-            val buf           = Array.ofDim[Byte](16 * 1024)
-            while ({
-              ent = zis.getNextEntry()
-              ent != null
-            })
-              if (!ent.isDirectory) {
-                val baos = new ByteArrayOutputStream
-                var read = -1
-                while ({
-                  read = zis.read(buf)
-                  read >= 0
-                })
-                  baos.write(buf, 0, read)
-                zis.closeEntry()
-                os.write(
-                  tmpRepoDir / ent.getName.split('/').toSeq,
-                  baos.toByteArray,
-                  createFolders = true
-                )
-              }
-          }
-          finally {
-            if (zis != null) zis.close()
-            if (is != null) is.close()
+          using(archiveUrl.openStream()) { is =>
+            using(new ZipInputStream(new BufferedInputStream(is))) { zis =>
+              extractZip(zis, tmpRepoDir)
+            }
           }
           os.move(tmpRepoDir, repoDir)
         }
 
+      val repo = "ivy:" + repoDir.toNIO.toUri.toASCIIString + "/[defaultPattern]"
       Some(repo)
     }
   }
