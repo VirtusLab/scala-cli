3b2a096875c8e7849008312db30dcae6c4ae5664
Łukasz Wroński <46607934+lwronski@users.noreply.github.com>
2021-10-11 20:16:52 +0200

    Add standalone launcher for Scala CLI - MacOs/Linux (#185)
    * Add standalone launcher for Scala CLI - macOs/linux

--- website/src/components/DownloadButton.js
+++ website/src/components/DownloadButton.js
@@ -15,7 +15,7 @@ class DownloadButton extends React.Component {
   render() {
     return (
       <button 
-      style={{height: '55px', width : '190px', fontSize: 15,  borderRadius: '3px', color: '#fff', background: '#DC332D', borderColor: '#DC332D', fontWeight: 500 }}
+      style={{height: '55px', width : this.props.width, fontSize: 15,  borderRadius: '3px', color: '#fff', background: '#DC332D', borderColor: '#DC332D', fontWeight: 500 }}
       onClick={this.handleClick}
       >
         {this.props.desc}
