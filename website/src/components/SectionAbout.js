b04afc8126834b3777b8303a05041abfb6caf78a
Krzysztof Romanowski <kromanowski@virtuslab.com>
2021-10-15 07:57:54 -0700

    Add scala-js and scala-native installation (#216)

--- website/src/components/SectionAbout.js
+++ website/src/components/SectionAbout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function SectionAbout(props){ 
-  return <div className="section-about__wrapper row">
-        <div className="col col--1 big-title pre-title">
-            &gt;_
-        </div>
+export default function SectionAbout(props){
+  const id = props.title.toLowerCase().split(" ").join("-")
+  const link = <a href={"#" + id} >&gt;_</a> 
+  return <div className="section-about__wrapper row" id={id}>
+        <div className="col col--1 big-title pre-title">{link}</div>
         <div className="col col--3 big-title">
-            <span className="pre-title-mobile">&gt;_</span> {props.title}
+            <span className="pre-title-mobile">{link}</span> {props.title}
         </div>
         <div className="col col--8 description">
             {props.children}
