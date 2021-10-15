52cb8168820e50db8e9dd2f02c94b163478ff2d9
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2021-10-15 18:34:17 +0200

    Download inputs each time (#214)

--- modules/cli/src/main/scala/scala/cli/commands/SharedOptions.scala
+++ modules/cli/src/main/scala/scala/cli/commands/SharedOptions.scala
@@ -13,8 +13,8 @@ import scala.build.blooprifle.BloopRifleConfig
 import scala.build.internal.Constants
 import scala.build.options._
 import scala.build.{Inputs, LocalRepo, Logger, Os, Positioned}
+import scala.concurrent.duration._
 import scala.util.Properties
-
 // format: off
 final case class SharedOptions(
   @Recurse
@@ -194,7 +194,7 @@ final case class SharedOptions(
     val download: String => Either[String, Array[Byte]] = { url =>
       val artifact = Artifact(url).withChanging(true)
       val res = coursierCache.logger.use {
-        coursierCache.file(artifact).run.unsafeRun()(coursierCache.ec)
+        coursierCache.withTtl(0.seconds).file(artifact).run.unsafeRun()(coursierCache.ec)
       }
       res
         .left.map(_.describe)
