06483ce33c18a17b28a3f55f738f81e44deaa6c6
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2021-10-20 17:31:28 +0200

    Build 'defaults' gif and 'powerful scala' gif fix

--- gifs/scenarios/defaults.sh
+++ gifs/scenarios/defaults.sh
@@ -8,7 +8,8 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 if [[ -z "${ASCIINEMA_REC}" ]]; then
   # Warm up scala-cli
-  echo "println(1)" | scala-cli -
+    echo "println(1)" | scala-cli -
+    scala-cli fmt .
   # or do other preparation (e.g. create code)
 else
   . $SCRIPT_DIR/../demo-magic.sh
@@ -16,9 +17,15 @@ else
   clear
 
   # Put your stuff here
-  pe "echo 'println(\"TODO: turn gifs/scenarios/defaults.sh into proper scenario showing Solid defaults\")' | scala-cli -"
-
-  # Wait a bit to read output of last command
+    cat <<EOF | updateFile Main.scala
+@main def hello() = {println("Hello "+"world")}
+EOF
   sleep 2
+  pe 'scala-cli .'
+  pe 'scala-cli fmt .'
+  p "cat Main.scala"
+  rougify --theme tulip Main.scala
+  pe 'scala-cli package .'
+  sleep 4
   echo " "
 fi
