66046134ce6d5da89a8e84c2ee1023dbd48556c3
gh-actions <actions@github.com>
2021-12-02 19:35:11 +0000

    Update scala-cli.sh launcher for 0.0.9

--- scala-cli.bat
+++ scala-cli.bat
@@ -7,7 +7,7 @@ rem Download the latest version of this script at https://github.com/VirtusLab/s
 
 setlocal enabledelayedexpansion
 
-set "SCALA_CLI_VERSION=0.0.8"
+set "SCALA_CLI_VERSION=0.0.9"
 
 set SCALA_CLI_URL=https://github.com/VirtusLab/scala-cli/releases/download/v%SCALA_CLI_VERSION%/scala-cli.bat
 set CACHE_BASE=%localappdata%/Coursier/v1
