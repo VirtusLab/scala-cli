eeef27c05675f0034f842384adb6a27d134ad39b
Łukasz Wroński <lukaszwronski98@gmail.com>
2021-12-06 21:12:46 +0100

    Add projects gif

--- gifs/scenarios/projects.sh
+++ gifs/scenarios/projects.sh
@@ -10,7 +10,8 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 if [[ -z "${ASCIINEMA_REC}" ]]; then
   # Warm up scala-cli
-  echo "println(1)" | scala-cli -
+  echo "println(1)" | scala-cli -S 2.13.6 -
+  echo "// using lib com.softwaremill.sttp.client3::core:3.3.18 " | scala-cli -S 2.13.6  -
   # or do other preparation (e.g. create code)
 else
   . $SCRIPT_DIR/../demo-magic.sh
@@ -18,9 +19,27 @@ else
   clearConsole
 
   # Put your stuff here
-  pe "echo 'println(\"TODO: turn gifs/scenarios/projects.sh into proper scenario Scala CLI to manage single-module projects\")' | scala-cli -"
+  cat <<EOF | updateFile Post.scala
+// using lib com.softwaremill.sttp.client3::core:3.3.18
+import sttp.client3._
+
+// https://sttp.softwaremill.com/en/latest/quickstart.html
+
+object Post extends App {
+  val backend = HttpURLConnectionBackend()
+  val response = basicRequest
+    .body("Hello, world!")  
+    .post(uri"https://httpbin.org/post?hello=world").send(backend)
+
+  println(response.body)  
+}
+EOF
+
+  doSleep 2
+
+  pe "scala-cli Post.scala -S 2.13.6"
 
   # Wait a bit to read output of last command
-  doSleep 2
+  doSleep 5
   echo " " && echo "ok" > status.txt
 fi
