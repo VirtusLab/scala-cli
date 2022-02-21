780cbe5f8b0a0fb67b8109942e47d71eebd8190a
zmerr <93259201+zmerr@users.noreply.github.com>
2022-02-21 13:10:12 +0300

    Update modules/build/src/main/scala/scala/build/errors/ScalaVersionError.scala
    Co-authored-by: Alexandre Archambault <alexarchambault@users.noreply.github.com>

--- modules/build/src/main/scala/scala/build/errors/ScalaVersionError.scala
+++ modules/build/src/main/scala/scala/build/errors/ScalaVersionError.scala
@@ -10,6 +10,6 @@ object ScalaVersionError {
     s"""You can only choose one of the 3.x, 2.13.x, and 2.12.x. versions.
        |The latest supported stable versions are ${latestSupportedStableVersions.mkString(", ")}.
        |In addition, you can request the latest Scala 2 and Scala 3 nightly versions by passing 2.nightly, and 3.nightly arguments respectively.
-       |For requesting a specific Scala 2 or Scala 3 nightly version, please specify the full version of the nightly without the repository argument.
+       |Specific Scala 2 or Scala 3 nightly versions are also accepted.
        |""".stripMargin
 }
