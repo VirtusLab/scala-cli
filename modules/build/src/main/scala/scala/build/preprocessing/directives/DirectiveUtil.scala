049233f2807512f165aa9a9aa0fe540d97769101
zmerr <ina.e.zimmer@gmail.com>
2022-02-22 16:37:21 +0300

    Added NumericValue to DirectiveUtil.stringValues

--- modules/build/src/main/scala/scala/build/preprocessing/directives/DirectiveUtil.scala
+++ modules/build/src/main/scala/scala/build/preprocessing/directives/DirectiveUtil.scala
@@ -1,6 +1,6 @@
 package scala.build.preprocessing.directives
 
-import com.virtuslab.using_directives.custom.model.{NumericValue, StringValue, Value}
+import com.virtuslab.using_directives.custom.model.{BooleanValue, NumericValue, StringValue, Value}
 
 import scala.build.preprocessing.ScopePath
 import scala.build.{Position, Positioned}
@@ -13,6 +13,7 @@ object DirectiveUtil {
   ): Seq[(Positioned[String], Option[ScopePath])] =
     values.collect {
       case v: StringValue =>
+        println("v.get as StringValue: "+ v.get())
         val line   = v.getRelatedASTNode.getPosition.getLine
         val column = v.getRelatedASTNode.getPosition.getColumn + 1 // Using directives are 0 based
         val pos    = Position.File(path, (line, column), (line, column))
@@ -20,6 +21,23 @@ object DirectiveUtil {
           Positioned(pos, v.get),
           Option(v.getScope).map((p: String) => cwd / os.RelPath(p))
         )
+      case v: NumericValue =>
+        println("v.get as NumericValue: "+ v.get())
+        val line   = v.getRelatedASTNode.getPosition.getLine
+        val column = v.getRelatedASTNode.getPosition.getColumn
+        val pos    = Position.File(path, (line, column), (line, column))
+        (
+          Positioned(pos, v.get),
+          Option(v.getScope).map((p: String) => cwd / os.RelPath(p))
+        )
+//      case v: BooleanValue =>
+//        val line   = v.getRelatedASTNode.getPosition.getLine
+//        val column = v.getRelatedASTNode.getPosition.getColumn
+//        val pos    = Position.File(path, (line, column), (line, column))
+//        (
+//          Positioned(pos, v.get),
+//          Option(v.getScope).map((p: String) => cwd / os.RelPath(p))
+//        )
     }
 
   def numericValues(
