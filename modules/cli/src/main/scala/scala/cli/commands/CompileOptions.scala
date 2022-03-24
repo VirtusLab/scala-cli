a493a0e95bf64b3fc02f1087a8cdf9b53cd48ad6
Krzysztof Romanowski <romanowski.kr@gmail.com>
2022-03-24 14:37:29 +0100

    Move CompileOptions methods to Compile

--- modules/cli/src/main/scala/scala/cli/commands/CompileOptions.scala
+++ modules/cli/src/main/scala/scala/cli/commands/CompileOptions.scala
@@ -3,10 +3,6 @@ package scala.cli.commands
 import caseapp._
 import caseapp.core.help.Help
 
-import scala.build.Os
-import scala.build.options.BuildOptions
-import scala.cli.commands.util.SharedOptionsUtil._
-
 // format: off
 @HelpMessage("Compile Scala code")
 final case class CompileOptions(
@@ -29,15 +25,8 @@ final case class CompileOptions(
 
   @HelpMessage("Compile test scope")
     test: Boolean = false
-) {
+)
   // format: on
-
-  def buildOptions: BuildOptions =
-    shared.buildOptions(enableJmh = false, jmhVersion = None)
-
-  def outputPath = output.filter(_.nonEmpty).map(p => os.Path(p, Os.pwd))
-
-}
 
 object CompileOptions {
   implicit lazy val parser: Parser[CompileOptions] = Parser.derive
