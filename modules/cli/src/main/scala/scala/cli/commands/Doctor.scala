82d3a3efa3165da3446b67b4e7784b6f4b2b7d44
Mark T. Kennedy <mtk@acm.org>
2022-03-26 13:20:29 -0400

    nothing substantive, really testing if i can push a commit

--- modules/cli/src/main/scala/scala/cli/commands/Doctor.scala
+++ modules/cli/src/main/scala/scala/cli/commands/Doctor.scala
@@ -5,13 +5,39 @@ import caseapp.core.RemainingArgs
 import scala.build.internal.Constants
 import scala.tools.nsc.io.File
 
+// current version / latest version + potentially information that
+// scala-cli should be updated (and that should take SNAPSHOT version
+// into account and mention that one is ahead of stable version)
+
+// the state of bloop (running/not running + version and JVM used)
+
+// if there are duplicated scala-cli on classpath
+
+// whether all native dependencies for native / js are installed
+
+// information about location of binary / main class that is being used
+
+// information if scala-cli can access Maven central / scala-cli
+// github with some tips and diagnostics about proxies
+// (@alexarchambault could you provide more details on what can be
+// printed?)
+
+// information if scala-cli is used as a native application or is using JVM
+
+
 object Doctor extends ScalaCommand[DoctorOptions] {
   override def group = "Doctor"
 
   def run(options: DoctorOptions, args: RemainingArgs): Unit = {
     checkIsVersionOutdated()
-    checkMultiplePath()
+    checkBloopStatus()
+    checkDuplicatesOnPath()
+    checkNativeDependencies()
+    checkJSDependencies()
+    //checkBinaryOrMainClass()??
+    checkAccessToMavneOrGithub()
     checkIsNativeOrJvm()
+    println("invisible!")
   }
 
   private def checkIsVersionOutdated(): Unit = {
@@ -23,21 +49,13 @@ object Doctor extends ScalaCommand[DoctorOptions] {
       )
     else
       println("scala-cli version is updated")
-
-    println()
   }
 
-  private def checkIsNativeOrJvm(): Unit = {
-    val jvmVersion = System.getProperty("java.vm.name")
-
-    if (jvmVersion.isEmpty)
-      println("scala-cli is used as a native application")
-    else
-      println(s"scala-cli using JVM : $jvmVersion")
-    println()
+  private def checkBloopStatus(): Unit = {
+    // TODO
   }
 
-  private def checkMultiplePath(): Unit = {
+  private def checkDuplicatesOnPath(): Unit = {
     val directories: Array[String] = System.getenv("PATH")
       .split(File.pathSeparator)
       .filter(_.isEmpty)
@@ -53,8 +71,28 @@ object Doctor extends ScalaCommand[DoctorOptions] {
       println(s"scala-cli installed on multiple paths ${scalaCliPaths.mkString(", ")} ")
     else
       println("scala-cli installed correctly")
-    println()
   }
+
+  private def checkNativeDependencies(): Unit = {
+  }
+
+  private def checkJSDependencies(): Unit = {
+  }
+
+    //checkBinaryOrMainClass()??
+
+  private def checkAccessToMavneOrGithub(): Unit = {
+  }
+
+  private def checkIsNativeOrJvm(): Unit = {
+    val jvmVersion = System.getProperty("java.vm.name")
+
+    if (jvmVersion.isEmpty)
+      println("scala-cli is used as a native application")
+    else
+      println(s"scala-cli using JVM : $jvmVersion")
+  }
+
 }
 
 
