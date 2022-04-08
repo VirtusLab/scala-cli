ee162eec3a0b3ce09a24b52d14f4d0fc34d7f5b3
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-04-08 15:09:03 +0200

    Hide the pgp* commands in native launcher
    Seems I had hidden those on the JVM, but not in native launchers.

--- modules/cli/src/main/scala/scala/cli/commands/pgp/PgpCreateExternal.scala
+++ modules/cli/src/main/scala/scala/cli/commands/pgp/PgpCreateExternal.scala
@@ -3,6 +3,7 @@ package scala.cli.commands.pgp
 import scala.cli.signing.commands.PgpCreateOptions
 
 class PgpCreateExternal extends PgpExternalCommand {
+  override def hidden = true
   def actualHelp      = PgpCreateOptions.help
   def externalCommand = Seq("pgp", "create")
 
