374ecf6a4808e5c52b7e34b3f317abb0b4e58b96
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-04-20 16:11:30 +0200

    Small refacto in UsingPublishDirectiveHandler

--- modules/directives/src/main/scala/scala/build/preprocessing/directives/UsingPublishDirectiveHandler.scala
+++ modules/directives/src/main/scala/scala/build/preprocessing/directives/UsingPublishDirectiveHandler.scala
@@ -1,5 +1,6 @@
 package scala.build.preprocessing.directives
 
+import scala.build.EitherCps.{either, value}
 import scala.build.Logger
 import scala.build.errors.{BuildException, UnexpectedDirectiveError}
 import scala.build.options.publish.{ComputeVersion, Developer, License, Vcs}
@@ -60,21 +61,28 @@ case object UsingPublishDirectiveHandler extends UsingDirectiveHandler {
   def handleValues(
     scopedDirective: ScopedDirective,
     logger: Logger
-  ): Either[BuildException, ProcessedUsingDirective] =
-    checkIfValuesAreExpected(scopedDirective).flatMap { groupedScopedValuesContainer =>
-      val severalValues = groupedScopedValuesContainer.scopedStringValues.map(_.positioned)
-      val singleValue   = severalValues.head
+  ): Either[BuildException, ProcessedUsingDirective] = either {
 
-      if (!scopedDirective.directive.key.startsWith(prefix))
-        Left(new UnexpectedDirectiveError(scopedDirective.directive.key))
-      else scopedDirective.directive.key.stripPrefix(prefix) match {
-        case "organization" =>
-          Right(PublishOptions(organization = Some(singleValue)))
-        case "name" | "moduleName" =>
-          Right(PublishOptions(name = Some(singleValue)))
-        case "version" =>
-          Right(PublishOptions(version = Some(singleValue)))
-        case "computeVersion" | "compute-version" =>
+    val groupedScopedValuesContainer = value(checkIfValuesAreExpected(scopedDirective))
+
+    val severalValues = groupedScopedValuesContainer.scopedStringValues.map(_.positioned)
+    val singleValue   = severalValues.head
+
+    val strippedKey =
+      if (scopedDirective.directive.key.startsWith(prefix))
+        scopedDirective.directive.key.stripPrefix(prefix)
+      else
+        value(Left(new UnexpectedDirectiveError(scopedDirective.directive.key)))
+
+    val publishOptions = strippedKey match {
+      case "organization" =>
+        PublishOptions(organization = Some(singleValue))
+      case "name" | "moduleName" =>
+        PublishOptions(name = Some(singleValue))
+      case "version" =>
+        PublishOptions(version = Some(singleValue))
+      case "computeVersion" | "compute-version" =>
+        value {
           ComputeVersion.parse(singleValue).map {
             computeVersion =>
               PublishOptions(
@@ -83,43 +91,49 @@ case object UsingPublishDirectiveHandler extends UsingDirectiveHandler {
                 )
               )
           }
-
-        case "url" =>
-          Right(PublishOptions(url = Some(singleValue)))
-        case "license" =>
+        }
+      case "url" =>
+        PublishOptions(url = Some(singleValue))
+      case "license" =>
+        value {
           License.parse(singleValue).map { license =>
             PublishOptions(license = Some(license))
           }
-        case "versionControl" | "version-control" | "scm" =>
+        }
+      case "versionControl" | "version-control" | "scm" =>
+        value {
           Vcs.parse(singleValue).map { versionControl =>
             PublishOptions(versionControl = Some(versionControl))
           }
-        case "description" =>
-          Right(PublishOptions(description = Some(singleValue.value)))
-        case "developer" =>
+        }
+      case "description" =>
+        PublishOptions(description = Some(singleValue.value))
+      case "developer" =>
+        value {
           Developer.parse(singleValue).map {
             developer => PublishOptions(developers = Seq(developer))
-
           }
-        case "scalaVersionSuffix" | "scala-version-suffix" =>
-          Right(PublishOptions(scalaVersionSuffix = Some(singleValue.value)))
-        case "scalaPlatformSuffix" | "scala-platform-suffix" =>
-          Right(PublishOptions(scalaPlatformSuffix = Some(singleValue.value)))
-        case "repository" =>
-          Right(PublishOptions(repository = Some(singleValue.value)))
-        case "gpgKey" | "gpg-key" =>
-          Right(PublishOptions(gpgSignatureId = Some(singleValue.value)))
-        case "gpgOptions" | "gpg-options" | "gpgOption" | "gpg-option" =>
-          Right(PublishOptions(gpgOptions = severalValues.map(_.value).toList))
-        case _ =>
-          Left(new UnexpectedDirectiveError(scopedDirective.directive.key))
-      }
-    }.map { publishOptions =>
-      val options = BuildOptions(
-        notForBloopOptions = PostBuildOptions(
-          publishOptions = publishOptions
-        )
-      )
-      ProcessedDirective(Some(options), Seq.empty)
+        }
+      case "scalaVersionSuffix" | "scala-version-suffix" =>
+        PublishOptions(scalaVersionSuffix = Some(singleValue.value))
+      case "scalaPlatformSuffix" | "scala-platform-suffix" =>
+        PublishOptions(scalaPlatformSuffix = Some(singleValue.value))
+      case "repository" =>
+        PublishOptions(repository = Some(singleValue.value))
+      case "gpgKey" | "gpg-key" =>
+        PublishOptions(gpgSignatureId = Some(singleValue.value))
+      case "gpgOptions" | "gpg-options" | "gpgOption" | "gpg-option" =>
+        PublishOptions(gpgOptions = severalValues.map(_.value).toList)
+      case _ =>
+        value(Left(new UnexpectedDirectiveError(scopedDirective.directive.key)))
     }
+
+    val options = BuildOptions(
+      notForBloopOptions = PostBuildOptions(
+        publishOptions = publishOptions
+      )
+    )
+
+    ProcessedDirective(Some(options), Seq.empty)
+  }
 }
