002b69c1388e2850912c058138bce1db9d15acc9
Piotr Chabelski <pchabelski@virtuslab.com>
2022-05-05 12:33:54 +0200

    Improve the `export` command messages   - fail early with a more descriptive message when the output path exists   - fail early if an export to multiple build tools is requested   - print the output path if the export is successful   - print a relevant information early if the export is successfully started (in case it loads too long)

--- modules/cli/src/main/scala/scala/cli/commands/Export.scala
+++ modules/cli/src/main/scala/scala/cli/commands/Export.scala
@@ -72,6 +72,29 @@ object Export extends ScalaCommand[ExportOptions] {
   def run(options: ExportOptions, args: RemainingArgs): Unit = {
     CurrentParams.verbosity = options.shared.logging.verbosity
     val logger = options.shared.logger
+
+    val output = options.output.getOrElse("dest")
+    val dest   = os.Path(output, os.pwd)
+    if (os.exists(dest)) {
+      System.err.println(
+        s"""Error: $dest already exists.
+           |To change the destination output directory pass --output path or remove the destination directory first.""".stripMargin
+      )
+      sys.exit(1)
+    }
+
+    val shouldExportToMill = options.mill.getOrElse(false)
+    val shouldExportToSbt  = options.sbt.getOrElse(false)
+    if (shouldExportToMill && shouldExportToSbt) {
+      System.err.println(
+        s"Error: Cannot export to both mill and sbt. please pick one build tool to export."
+      )
+      sys.exit(1)
+    }
+
+    val buildToolName = if (shouldExportToMill) "mill" else "sbt"
+    System.out.println(s"Exporting to a $buildToolName project...")
+
     val inputs = options.shared.inputsOrExit(args)
     CurrentParams.workspaceOpt = Some(inputs.workspace)
     val baseOptions =
@@ -112,23 +135,15 @@ object Export extends ScalaCommand[ExportOptions] {
       sbtBuildTool(options.sbtSetting.map(_.trim).filter(_.nonEmpty), sbtVersion, logger)
 
     val buildTool =
-      if (options.sbt.getOrElse(false))
-        sbtBuildTool0
-      else if (options.mill.getOrElse(false))
+      if (shouldExportToMill)
         millBuildTool(options.shared.coursierCache, logger)
-      else
+      else // shouldExportToSbt isn't checked, as it's treated as default
         sbtBuildTool0
 
     val project = buildTool.`export`(optionsMain0, optionsTest0, sourcesMain, sourcesTest)
 
-    val output = options.output.getOrElse("dest")
-    val dest   = os.Path(output, os.pwd)
-    if (os.exists(dest)) {
-      System.err.println(s"Error: $output already exists.")
-      sys.exit(1)
-    }
-
     os.makeDir.all(dest)
     project.writeTo(dest)
+    System.out.println(s"Exported to: $dest")
   }
 }
