88a5e05b53f6aab6691146240c1b64cf7d87b679
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-05-19 18:16:45 +0200

    Add throwaway pgp key creation capabilities

---
+++ modules/cli/src/main/scala/scala/cli/commands/config/ThrowawayPgpSecret.scala
@@ -0,0 +1,70 @@
+package scala.cli.commands.config
+
+import coursier.cache.Cache
+import coursier.util.Task
+
+import java.security.SecureRandom
+
+import scala.build.EitherCps.{either, value}
+import scala.build.Logger
+import scala.build.errors.BuildException
+import scala.cli.commands.pgp.PgpProxyMaker
+import scala.cli.errors.PgpError
+import scala.cli.signing.shared.Secret
+
+object ThrowawayPgpSecret {
+
+  private val secretChars =
+    (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9') ++ Seq('$', '/', '*', '&', '\'', '"', '!', '(',
+      ')', '-', '_', '\\', ';', '.', ':', '=', '+', '?', ',', '%')).toVector
+  private def secretChars(rng: SecureRandom): Iterator[Char] =
+    Iterator.continually {
+      val idx = rng.nextInt(secretChars.length)
+      secretChars(idx)
+    }
+
+  def pgpPassPhrase(): Secret[String] = {
+    val random = new SecureRandom
+    Secret(secretChars(random).take(32).mkString)
+  }
+  def pgpSecret(
+    mail: String,
+    password: Secret[String],
+    logger: Logger,
+    cache: Cache[Task]
+  ): Either[BuildException, (Secret[String], Secret[Array[Byte]])] = either {
+
+    val dir    = os.temp.dir(perms = "rwx------")
+    val pubKey = dir / "pub"
+    val secKey = dir / "sec"
+    val retCode = value {
+      (new PgpProxyMaker).get().createKey(
+        pubKey.toString,
+        secKey.toString,
+        mail,
+        logger.verbosity <= 0,
+        password.value,
+        cache,
+        logger
+      )
+    }
+
+    def cleanUp(): Unit =
+      os.remove.all(dir)
+
+    if (retCode == 0)
+      try (Secret(os.read(pubKey)), Secret(os.read.bytes(secKey)))
+      finally cleanUp()
+    else {
+      cleanUp()
+      value {
+        Left {
+          new PgpError(
+            s"Failed to create PGP key pair (see messages above, scala-cli-signing return code: $retCode)"
+          )
+        }
+      }
+    }
+  }
+
+}
--- modules/cli/src/main/scala/scala/cli/commands/pgp/PgpProxy.scala
+++ modules/cli/src/main/scala/scala/cli/commands/pgp/PgpProxy.scala
@@ -5,11 +5,42 @@ import coursier.util.Task
 
 import scala.build.Logger
 import scala.build.errors.BuildException
-import scala.cli.commands.pgp.PgpKeyIdExternal
+import scala.cli.commands.pgp.{PgpCreateExternal, PgpKeyIdExternal}
 import scala.cli.errors.PgpError
 import scala.util.Properties
 
 class PgpProxy {
+  def createKey(
+    pubKey: String,
+    secKey: String,
+    mail: String,
+    quiet: Boolean,
+    password: String,
+    cache: Cache[Task],
+    logger: Logger
+  ): Either[BuildException, Int] = {
+    val quietOptions = Nil
+    (new PgpCreateExternal).tryRun(
+      cache,
+      None,
+      Seq(
+        "pgp",
+        "create",
+        "--pub-dest",
+        pubKey.toString,
+        "--secret-dest",
+        secKey.toString,
+        "--email",
+        mail,
+        "--password",
+        s"env:SCALA_CLI_RANDOM_KEY_PASSWORD"
+      ) ++ quietOptions,
+      Map("SCALA_CLI_RANDOM_KEY_PASSWORD" -> password),
+      logger,
+      allowExecve = false
+    )
+  }
+
   def keyId(
     key: String,
     keyPrintablePath: String,
@@ -39,4 +70,5 @@ class PgpProxy {
         Right(rawOutput)
     }
   }
+
 }
