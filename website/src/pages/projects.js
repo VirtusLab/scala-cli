e46b73b26327a2889ae6df70c348d0938708a32c
Olivier Mélois <baccata64@gmail.com>
2022-06-08 14:10:17 +0200

    Some typos & rewording on the single-module projects use case page (#1089)
    PR raised via github so I reviewed a single page, but I needed to scratch that itch

--- website/src/pages/projects.js
+++ website/src/pages/projects.js
@@ -5,12 +5,12 @@ import UseCase from "../components/UseCase"
 const Index = (props) => {
   return <UseCase
     title="Single-module projects with Scala CLI"
-    description="Page describing why Scala CLI is good for maintainig a single-module projects."
-    headline="Fight with your bugs not with with you buildtool"
+    description="Page describing why Scala CLI is good for maintaining single-module projects."
+    headline="Fight with your bugs, not with your buildtool"
     image="gifs/projects.gif"
     id="projects"
     >
-    <p>Scala CLI provides all tools to maintain with easy single module projects like cli apps or single microservice.</p>
+    <p>Scala CLI provides all the functionality to easily maintain single module projects like cli apps or simple microservices.</p>
   </UseCase>;
 };
 
