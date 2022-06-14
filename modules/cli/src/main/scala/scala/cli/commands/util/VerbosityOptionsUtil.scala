9eb3bb1f9d68ee623bf61b901fa7189bb4b72acf
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-06-14 14:22:43 +0200

    Fixes after review

--- modules/cli/src/main/scala/scala/cli/commands/util/VerbosityOptionsUtil.scala
+++ modules/cli/src/main/scala/scala/cli/commands/util/VerbosityOptionsUtil.scala
@@ -6,6 +6,6 @@ import scala.build.interactive.Interactive._
 object VerbosityOptionsUtil {
   implicit class VerbosityOptionsOps(v: VerbosityOptions) {
     def interactiveInstance(forceEnable: Boolean = false) =
-      if (v.interactive.getOrElse(false) || forceEnable) InteractiveAsk else InteractiveNop
+      if (v.interactive.getOrElse(forceEnable)) InteractiveAsk else InteractiveNop
   }
 }
