f5b5ffbe9fbe011bbb9f43b410c2d7d5facc717b
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-06-14 16:01:09 +0200

    Make redirection of Bloop output to file more robust
    Previously, we were calling redirectOutput / redirectError to redirect
    the Bloop output to a (single) file. But the resulting output file
    seemed to contain truncated lines, or to be missing some output. Same
    with redirectErrorStream(true). My guess is that the JVM was opening the
    output file twice (once for stdout, once for stderr), and writing to the
    two file descriptors at the same time was a problem.
    
    With these changes, we start the Bloop server via a shell script, and
    make it do "> output-file 2>&1". The shell handles redirecting both
    stdout and stderr to the same file better it seems, opening the file
    only once under-the-hood I think.
    
    Rather than doing this via a shell script, we could try to do the same
    as what the shell does, by calling ourselves system calls to open the
    output file (open), duplicating it (dup2?), then fork / exec. That's
    more cumbersome (we'd likely have to rely on JNI, or JNA + GraalVM API,
    to have it work both from the JVM and the native launcher), so we should
    probably do it only if ever using the shell ends up being a problem IMO…

--- modules/build/src/test/scala/scala/build/tests/util/BloopServer.scala
+++ modules/build/src/test/scala/scala/build/tests/util/BloopServer.scala
@@ -4,6 +4,7 @@ import coursier.cache.FileCache
 
 import scala.build.{Bloop, Logger}
 import scala.build.blooprifle.BloopRifleConfig
+import scala.util.Properties
 
 object BloopServer {
 
@@ -15,10 +16,26 @@ object BloopServer {
   private def bloopAddress =
     BloopRifleConfig.Address.DomainSocket(directories.bloopDaemonDir.toNIO)
 
-  val bloopConfig = BloopRifleConfig.default(
-    bloopAddress,
-    v => Bloop.bloopClassPath(Logger.nop, FileCache(), v),
-    directories.bloopWorkingDir.toIO
-  )
+  val bloopConfig = {
+    val base = BloopRifleConfig.default(
+      bloopAddress,
+      v => Bloop.bloopClassPath(Logger.nop, FileCache(), v),
+      directories.bloopWorkingDir.toIO
+    )
+    base.copy(
+      javaPath =
+        if (Properties.isWin) base.javaPath
+        else
+          // On Linux / macOS, we start the Bloop server via /bin/sh,
+          // which can have issues with the directory of "java" in the PATH,
+          // if it contains '+' or '%' IIRC.
+          // So we hardcode the path to "java" here.
+          Option(System.getenv("JAVA_HOME"))
+            .map(os.Path(_, os.pwd))
+            .map(_ / "bin" / "java")
+            .map(_.toString)
+            .getOrElse(base.javaPath)
+    )
+  }
 
 }
