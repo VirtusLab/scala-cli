6d96a8ce8768286707c954bd4ab04b76a5217867
Scala Steward <scala_steward@virtuslab.com>
2022-06-27 14:07:01 +0000

    Update test-runner, tools to 0.4.5

--- modules/core/src/main/scala/scala/build/errors/ScalaNativeCompatibilityError.scala
+++ modules/core/src/main/scala/scala/build/errors/ScalaNativeCompatibilityError.scala
@@ -6,7 +6,7 @@ final class ScalaNativeCompatibilityError(
 ) extends BuildException(
       s"""Used Scala Native version $scalaNativeVersion is incompatible with Scala $scalaVersion.
          |Please try one of the following combinations:
-         |  Scala Native version >= 0.4.4 for Scala 3.1 (*.sc & *.scala files)
+         |  Scala Native version >= 0.4.5 for Scala 3.1 (*.sc & *.scala files)
          |  Scala Native version >= 0.4.0 for Scala 2.13 (*.sc & *.scala files)
          |  Scala Native version >= 0.4.0 for Scala 2.12 (*.scala files)
          |Windows is supported since Scala Native 0.4.1.
