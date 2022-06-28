8b9663fdb039bb924f0071324ef0dcc41773d2c2
Piotr Chabelski <pchabelski@virtuslab.com>
2022-06-28 09:26:55 +0200

    Add more tests for the `fmt` command

--- modules/integration/src/test/scala/scala/cli/integration/FmtTests.scala
+++ modules/integration/src/test/scala/scala/cli/integration/FmtTests.scala
@@ -4,6 +4,8 @@ import com.eed3si9n.expecty.Expecty.expect
 
 class FmtTests extends munit.FunSuite {
 
+  val emptyInputs: TestInputs = TestInputs(Seq(os.rel / ".placeholder" -> ""))
+
   val simpleInputsUnformattedContent: String =
     """package foo
       |
@@ -28,6 +30,19 @@ class FmtTests extends munit.FunSuite {
       |}
       |""".stripMargin
   }
+
+  val simpleInputsWithFilter: TestInputs = TestInputs(
+    Seq(
+      os.rel / ".scalafmt.conf" ->
+        s"""|version = "${Constants.defaultScalafmtVersion}"
+            |runner.dialect = scala213
+            |project.excludePaths = [ "glob:**/should/not/format/**.scala" ]
+            |""".stripMargin,
+      os.rel / "Foo.scala"                 -> expectedSimpleInputsFormattedContent,
+      os.rel / "scripts" / "SomeScript.sc" -> "println()\n",
+      os.rel / "should" / "not" / "format" / "ShouldNotFormat.scala" -> simpleInputsUnformattedContent
+    )
+  )
 
   private def noCrLf(input: String): String =
     input.replaceAll("\r\n", "\n")
@@ -54,6 +69,30 @@ class FmtTests extends munit.FunSuite {
       val updatedContent = noCrLf(os.read(root / "Foo.scala"))
       expect(updatedContent == noCrLf(simpleInputsUnformattedContent))
       expect(noCrLf(out) == "error: --test failed\n")
+    }
+  }
+
+  test("filter correctly with --check") {
+    simpleInputsWithFilter.fromRoot { root =>
+      val out      = os.proc(TestUtil.cli, "fmt", ".", "--check").call(cwd = root).out.text.trim
+      val outLines = out.linesIterator.toSeq
+      expect(outLines.length == 2)
+      expect(outLines.head == "Looking for unformatted files...")
+      expect(outLines.last == "All files are formatted with scalafmt :)")
+    }
+  }
+
+  test("--scalafmt-help") {
+    emptyInputs.fromRoot { root =>
+      val out1 = os.proc(TestUtil.cli, "fmt", "--scalafmt-help").call(cwd = root).out.text.trim
+      val out2 = os.proc(TestUtil.cli, "fmt", "-F", "--help").call(cwd = root).out.text.trim
+      expect(out1.nonEmpty)
+      expect(out1 == out2)
+      val outLines       = out1.linesIterator.toSeq
+      val outVersionLine = outLines.head
+      expect(outVersionLine == s"scalafmt ${Constants.defaultScalafmtVersion}")
+      val outUsageLine = outLines.drop(1).head
+      expect(outUsageLine == "Usage: scalafmt [options] [<file>...]")
     }
   }
 
