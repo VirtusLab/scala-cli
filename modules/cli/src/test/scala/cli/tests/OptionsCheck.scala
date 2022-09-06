05a22610c07702d161712df4eb82e6b93f72edcc
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-09-06 17:23:45 +0200

    Check for duplicated arguments in default command too

--- modules/cli/src/test/scala/cli/tests/OptionsCheck.scala
+++ modules/cli/src/test/scala/cli/tests/OptionsCheck.scala
@@ -4,7 +4,10 @@ import scala.cli.ScalaCliCommands
 
 class OptionsCheck extends munit.FunSuite {
 
-  for (command <- (new ScalaCliCommands("scala-cli", isSipScala = false)).commands)
+  private val helper = new ScalaCliCommands("scala-cli", isSipScala = false)
+  val commands = helper.defaultCommand.toSeq ++ helper.commands
+
+  for (command <- commands)
     test(s"No duplicated options in ${command.names.head.mkString(" ")}") {
       command.ensureNoDuplicates()
     }
