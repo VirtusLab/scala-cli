5558dee8944c0422288eea4d52ff1996701c4b12
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-09-22 12:02:33 +0200

    Add HTTP proxy-related keys in config module

--- modules/config/src/main/scala/scala/cli/config/Keys.scala
+++ modules/config/src/main/scala/scala/cli/config/Keys.scala
@@ -19,6 +19,10 @@ object Keys {
 
   val interactive = new Key.BooleanEntry(Seq.empty, "interactive")
 
+  val proxyAddress  = new Key.StringEntry(Seq("httpProxy"), "address")
+  val proxyUser     = new Key.PasswordEntry(Seq("httpProxy"), "user")
+  val proxyPassword = new Key.PasswordEntry(Seq("httpProxy"), "password")
+
   // setting indicating if the global interactive mode was suggested
   val globalInteractiveWasSuggested = new Key.BooleanEntry(Seq.empty, "interactive-was-suggested")
 
@@ -34,7 +38,10 @@ object Keys {
     pgpSecretKeyPassword,
     pgpPublicKey,
     sonatypeUser,
-    sonatypePassword
+    sonatypePassword,
+    proxyAddress,
+    proxyUser,
+    proxyPassword
   )
 
   lazy val map: Map[String, Key[_]] = all.map(e => e.fullName -> e).toMap
