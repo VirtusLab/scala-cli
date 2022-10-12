7c650dc06cc94c86a826bffea7de26e85f9af5d9
Krzysztof Romanowski <romanowski.kr@gmail.com>
2022-10-12 14:26:23 +0200

    Define specification levels

---
+++ modules/cli-options/src/main/scala/scala/cli/commands/SpecificationLevel.scala
@@ -0,0 +1,40 @@
+package scala.cli.commands
+
+import caseapp.annotation.Tag
+import os.copy
+
+sealed trait SpecificationLevel {
+  def md = toString() + " have"
+}
+object SpecificationLevel {
+  case object MUST   extends SpecificationLevel
+  case object SHOULD extends SpecificationLevel
+  case object IMPLEMENTATION extends SpecificationLevel {
+    override def md = toString() + " specific"
+  }
+  case object RESTRICTED extends SpecificationLevel {
+    override def md = "Scala CLI specific"
+  }
+  case object EXPERIMENTAL extends SpecificationLevel {
+    override def md: String = toString()
+  }
+
+  val inSpecification = Seq(MUST, SHOULD)
+}
+
+object tags {
+  val experimental   = SpecificationLevel.EXPERIMENTAL.toString()
+  val restricted     = SpecificationLevel.RESTRICTED.toString()
+  val implementation = SpecificationLevel.IMPLEMENTATION.toString()
+  val must           = SpecificationLevel.MUST.toString()
+  val should         = SpecificationLevel.SHOULD.toString()
+
+  def levelFor(name: String) = name match {
+    case `experimental`   => Some(SpecificationLevel.EXPERIMENTAL)
+    case `restricted`     => Some(SpecificationLevel.RESTRICTED)
+    case `implementation` => Some(SpecificationLevel.IMPLEMENTATION)
+    case `must`           => Some(SpecificationLevel.MUST)
+    case `should`         => Some(SpecificationLevel.SHOULD)
+    case _                => None
+  }
+}
