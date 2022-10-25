13056de4597ddc191193b9fb1afe7cbd4087fae2
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2022-10-25 11:23:06 +0200

    No need to use os.ProcessOutput.ReadLines in test (#1491)

--- modules/integration/src/test/scala/scala/cli/integration/ReplTests.scala
+++ modules/integration/src/test/scala/scala/cli/integration/ReplTests.scala
@@ -2,8 +2,6 @@ package scala.cli.integration
 
 import com.eed3si9n.expecty.Expecty.expect
 
-import scala.collection.mutable
-
 class ReplTests extends ScalaCliSuite {
 
   test("calling repl with -Xshow-phases flag") {
@@ -13,11 +11,9 @@ class ReplTests extends ScalaCliSuite {
       "-Xshow-phases"
     )
 
-    val builder   = new mutable.StringBuilder
-    val readLines = os.ProcessOutput.Readlines(s => builder.append(s))
-    val res       = os.proc(cmd).call(stdout = readLines, stderr = readLines)
+    val res = os.proc(cmd).call(mergeErrIntoOut = true)
     expect(res.exitCode == 0)
-    val output = builder.mkString
+    val output = res.out.text()
     expect(output.contains("parser") && output.contains("typer"))
   }
 
