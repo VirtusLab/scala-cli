5ac860c4c3ba8ecf2547f6925a69b945a892f388
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-12-01 16:14:29 +0100

    Add integration test to run scala nightly version

--- modules/integration/src/test/scala/scala/cli/integration/RunTests213.scala
+++ modules/integration/src/test/scala/scala/cli/integration/RunTests213.scala
@@ -1,7 +1,27 @@
 package scala.cli.integration
 
+import com.eed3si9n.expecty.Expecty.expect
+
 // format: off
 class RunTests213 extends RunTestDefinitions(
   scalaVersionOpt = Some(Constants.scala213)
-)
+){
 // format: on
+  test("2.13.nightly") {
+    TestInputs(os.rel / "sample.sc" -> "println(util.Properties.versionNumberString)").fromRoot {
+      root =>
+        val res =
+          os.proc(
+            TestUtil.cli,
+            "run",
+            ".",
+            "-S",
+            "2.13.nightly",
+            TestUtil.extraOptions
+          )
+            .call(cwd = root)
+        val version = res.out.trim()
+        expect(version.startsWith("2.13"))
+    }
+  }
+}
