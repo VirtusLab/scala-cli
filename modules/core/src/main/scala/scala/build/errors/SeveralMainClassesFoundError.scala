cca3fa95877dd7b6008977ac73b0c1d7203d65af
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-12-19 11:48:08 +0100

    Sort main classes earlier, don't sort them later in error

--- modules/core/src/main/scala/scala/build/errors/SeveralMainClassesFoundError.scala
+++ modules/core/src/main/scala/scala/build/errors/SeveralMainClassesFoundError.scala
@@ -7,17 +7,11 @@ final class SeveralMainClassesFoundError(
   commandString: String,
   positions: Seq[Position]
 ) extends MainClassError(
-      {
-        val sortedMainClasses = mainClasses.sorted
-        val mainClassesString = sortedMainClasses.mkString(", ")
-        s"""Found several main classes: $mainClassesString
-           |${sortedMainClasses.headOption.map(mc =>
-            s"""You can run one of them by passing it with the --main-class option, e.g.
-               |  ${Console.BOLD}$commandString --main-class $mc${Console.RESET}
-               |""".stripMargin
-          ).getOrElse("")}
-           |You can pick the main class interactively by passing the --interactive option.
-           |  ${Console.BOLD}$commandString --interactive${Console.RESET}""".stripMargin
-      },
+      s"""Found several main classes: ${mainClasses.mkString(", ")}
+         |You can run one of them by passing it with the --main-class option, e.g.
+         |  ${Console.BOLD}$commandString --main-class ${mainClasses.head}${Console.RESET}
+         |
+         |You can pick the main class interactively by passing the --interactive option.
+         |  ${Console.BOLD}$commandString --interactive${Console.RESET}""".stripMargin,
       positions = positions
     )
