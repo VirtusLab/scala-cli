97ccb9d1b3867149e235cf8b4a1ef0079ff72c8a
Piotr Chabelski <ged.subfan@gmail.com>
2023-01-11 13:51:47 +0100

    NIT Extract legacy `scala` runner tests to separate trait

--- modules/integration/src/test/scala/scala/cli/integration/DefaultTests.scala
+++ modules/integration/src/test/scala/scala/cli/integration/DefaultTests.scala
@@ -2,7 +2,7 @@ package scala.cli.integration
 
 import com.eed3si9n.expecty.Expecty.expect
 
-class DefaultTests extends WithWarmUpScalaCliSuite {
+class DefaultTests extends WithWarmUpScalaCliSuite with LegacyScalaRunnerTestDefinitions {
   override def warmUpExtraTestOptions: Seq[String] = TestUtil.extraOptions
 
   test("running scala-cli with no args should default to repl") {
@@ -33,17 +33,6 @@ class DefaultTests extends WithWarmUpScalaCliSuite {
       )
       expect(res.exitCode == 1)
       expect(res.out.lines().endsWith(unrecognizedArgMessage(replSpecificOption)))
-    }
-  }
-
-  test("default to the run sub-command when a script snippet is passed with -e") {
-    TestInputs.empty.fromRoot { root =>
-      val msg       = "Hello world"
-      val quotation = TestUtil.argQuotationMark
-      val res =
-        os.proc(TestUtil.cli, "-e", s"println($quotation$msg$quotation)", TestUtil.extraOptions)
-          .call(cwd = root)
-      expect(res.out.trim() == msg)
     }
   }
 
@@ -96,23 +85,6 @@ class DefaultTests extends WithWarmUpScalaCliSuite {
         )
           .call(cwd = root)
       expect(res.out.trim() == msg)
-    }
-  }
-
-  test("running scala-cli with a script snippet passed with -e shouldn't allow repl-only options") {
-    TestInputs.empty.fromRoot { root =>
-      val replSpecificOption = "--repl-dry-run"
-      val res =
-        os.proc(
-          TestUtil.cli,
-          "-e",
-          "println()",
-          replSpecificOption,
-          TestUtil.extraOptions
-        )
-          .call(cwd = root, mergeErrIntoOut = true, check = false)
-      expect(res.exitCode == 1)
-      expect(res.out.lines().endsWith(unrecognizedArgMessage(replSpecificOption)))
     }
   }
 
@@ -183,65 +155,7 @@ class DefaultTests extends WithWarmUpScalaCliSuite {
     }
   }
 
-  test("ensure -save/--save works with the default command") {
-    val msg = "Hello world"
-    TestInputs(os.rel / "s.sc" -> s"""println("$msg")""").fromRoot { root =>
-      val legacySaveOption = "-save"
-      val res1 =
-        os.proc(TestUtil.cli, ".", legacySaveOption, TestUtil.extraOptions)
-          .call(cwd = root, stderr = os.Pipe)
-      expect(res1.out.trim() == msg)
-      expect(res1.err.trim().contains(s"Deprecated option '$legacySaveOption' is ignored"))
-      val doubleDashSaveOption = "--save"
-      val res2 =
-        os.proc(TestUtil.cli, ".", doubleDashSaveOption, TestUtil.extraOptions)
-          .call(cwd = root, stderr = os.Pipe)
-      expect(res2.out.trim() == msg)
-      expect(res2.err.trim().contains(s"Deprecated option '$doubleDashSaveOption' is ignored"))
-    }
-  }
-
-  test("ensure -nosave/--nosave works with the default command") {
-    val msg = "Hello world"
-    TestInputs(os.rel / "s.sc" -> s"""println("$msg")""").fromRoot { root =>
-      val legacyNoSaveOption = "-nosave"
-      val res1 =
-        os.proc(TestUtil.cli, ".", legacyNoSaveOption, TestUtil.extraOptions)
-          .call(cwd = root, stderr = os.Pipe)
-      expect(res1.out.trim() == msg)
-      expect(res1.err.trim().contains(s"Deprecated option '$legacyNoSaveOption' is ignored"))
-      val doubleDashNoSaveOption = "--nosave"
-      val res2 =
-        os.proc(TestUtil.cli, ".", doubleDashNoSaveOption, TestUtil.extraOptions)
-          .call(cwd = root, stderr = os.Pipe)
-      expect(res2.out.trim() == msg)
-      expect(res2.err.trim().contains(s"Deprecated option '$doubleDashNoSaveOption' is ignored"))
-    }
-  }
-
-  test("ensure -howtorun/--how-to-run works with the default command") {
-    val msg = "Hello world"
-    TestInputs(os.rel / "s.sc" -> s"""println("$msg")""").fromRoot { root =>
-      Seq("object", "script", "jar", "repl", "guess", "invalid").foreach { htrValue =>
-        val legacyHtrOption = "-howtorun"
-        val res1 =
-          os.proc(TestUtil.cli, ".", legacyHtrOption, htrValue, TestUtil.extraOptions)
-            .call(cwd = root, stderr = os.Pipe)
-        expect(res1.out.trim() == msg)
-        expect(res1.err.trim().contains(s"Deprecated option '$legacyHtrOption' is ignored"))
-        expect(res1.err.trim().contains(htrValue))
-        val doubleDashHtrOption = "--how-to-run"
-        val res2 =
-          os.proc(TestUtil.cli, ".", doubleDashHtrOption, htrValue, TestUtil.extraOptions)
-            .call(cwd = root, stderr = os.Pipe)
-        expect(res2.out.trim() == msg)
-        expect(res2.err.trim().contains(s"Deprecated option '$doubleDashHtrOption' is ignored"))
-        expect(res2.err.trim().contains(htrValue))
-      }
-    }
-  }
-
-  private def unrecognizedArgMessage(argName: String) =
+  protected def unrecognizedArgMessage(argName: String): Vector[String] =
     s"""
        |Unrecognized argument: $argName
        |
@@ -249,5 +163,5 @@ class DefaultTests extends WithWarmUpScalaCliSuite {
        |  ${Console.BOLD}${TestUtil.detectCliPath} --help${Console.RESET}
        |""".stripMargin.trim.linesIterator.toVector
 
-  private lazy val replDryRunOutput = "Dry run, not running REPL."
+  protected lazy val replDryRunOutput = "Dry run, not running REPL."
 }
---
+++ modules/integration/src/test/scala/scala/cli/integration/LegacyScalaRunnerTestDefinitions.scala
@@ -0,0 +1,90 @@
+package scala.cli.integration
+
+import com.eed3si9n.expecty.Expecty.expect
+trait LegacyScalaRunnerTestDefinitions { _: DefaultTests =>
+  test("default to the run sub-command when a script snippet is passed with -e") {
+    TestInputs.empty.fromRoot { root =>
+      val msg       = "Hello world"
+      val quotation = TestUtil.argQuotationMark
+      val res =
+        os.proc(TestUtil.cli, "-e", s"println($quotation$msg$quotation)", TestUtil.extraOptions)
+          .call(cwd = root)
+      expect(res.out.trim() == msg)
+    }
+  }
+
+  test("running scala-cli with a script snippet passed with -e shouldn't allow repl-only options") {
+    TestInputs.empty.fromRoot { root =>
+      val replSpecificOption = "--repl-dry-run"
+      val res =
+        os.proc(
+          TestUtil.cli,
+          "-e",
+          "println()",
+          replSpecificOption,
+          TestUtil.extraOptions
+        )
+          .call(cwd = root, mergeErrIntoOut = true, check = false)
+      expect(res.exitCode == 1)
+      expect(res.out.lines().endsWith(unrecognizedArgMessage(replSpecificOption)))
+    }
+  }
+
+  test("ensure -save/--save works with the default command") {
+    val msg = "Hello world"
+    TestInputs(os.rel / "s.sc" -> s"""println("$msg")""").fromRoot { root =>
+      val legacySaveOption = "-save"
+      val res1 =
+        os.proc(TestUtil.cli, ".", legacySaveOption, TestUtil.extraOptions)
+          .call(cwd = root, stderr = os.Pipe)
+      expect(res1.out.trim() == msg)
+      expect(res1.err.trim().contains(s"Deprecated option '$legacySaveOption' is ignored"))
+      val doubleDashSaveOption = "--save"
+      val res2 =
+        os.proc(TestUtil.cli, ".", doubleDashSaveOption, TestUtil.extraOptions)
+          .call(cwd = root, stderr = os.Pipe)
+      expect(res2.out.trim() == msg)
+      expect(res2.err.trim().contains(s"Deprecated option '$doubleDashSaveOption' is ignored"))
+    }
+  }
+
+  test("ensure -nosave/--nosave works with the default command") {
+    val msg = "Hello world"
+    TestInputs(os.rel / "s.sc" -> s"""println("$msg")""").fromRoot { root =>
+      val legacyNoSaveOption = "-nosave"
+      val res1 =
+        os.proc(TestUtil.cli, ".", legacyNoSaveOption, TestUtil.extraOptions)
+          .call(cwd = root, stderr = os.Pipe)
+      expect(res1.out.trim() == msg)
+      expect(res1.err.trim().contains(s"Deprecated option '$legacyNoSaveOption' is ignored"))
+      val doubleDashNoSaveOption = "--nosave"
+      val res2 =
+        os.proc(TestUtil.cli, ".", doubleDashNoSaveOption, TestUtil.extraOptions)
+          .call(cwd = root, stderr = os.Pipe)
+      expect(res2.out.trim() == msg)
+      expect(res2.err.trim().contains(s"Deprecated option '$doubleDashNoSaveOption' is ignored"))
+    }
+  }
+
+  test("ensure -howtorun/--how-to-run works with the default command") {
+    val msg = "Hello world"
+    TestInputs(os.rel / "s.sc" -> s"""println("$msg")""").fromRoot { root =>
+      Seq("object", "script", "jar", "repl", "guess", "invalid").foreach { htrValue =>
+        val legacyHtrOption = "-howtorun"
+        val res1 =
+          os.proc(TestUtil.cli, ".", legacyHtrOption, htrValue, TestUtil.extraOptions)
+            .call(cwd = root, stderr = os.Pipe)
+        expect(res1.out.trim() == msg)
+        expect(res1.err.trim().contains(s"Deprecated option '$legacyHtrOption' is ignored"))
+        expect(res1.err.trim().contains(htrValue))
+        val doubleDashHtrOption = "--how-to-run"
+        val res2 =
+          os.proc(TestUtil.cli, ".", doubleDashHtrOption, htrValue, TestUtil.extraOptions)
+            .call(cwd = root, stderr = os.Pipe)
+        expect(res2.out.trim() == msg)
+        expect(res2.err.trim().contains(s"Deprecated option '$doubleDashHtrOption' is ignored"))
+        expect(res2.err.trim().contains(htrValue))
+      }
+    }
+  }
+}
