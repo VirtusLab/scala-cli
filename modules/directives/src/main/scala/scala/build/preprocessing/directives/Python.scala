60a92b1c7148dff7de31bc6ed1a5700210f15aa5
Alexandre Archambault <alexandre.archambault@gmail.com>
2023-01-30 16:24:33 +0100

    Unused import

--- modules/directives/src/main/scala/scala/build/preprocessing/directives/Python.scala
+++ modules/directives/src/main/scala/scala/build/preprocessing/directives/Python.scala
@@ -3,7 +3,6 @@ package scala.build.preprocessing.directives
 import scala.build.directives.*
 import scala.build.errors.BuildException
 import scala.build.options.{BuildOptions, PostBuildOptions}
-import scala.build.preprocessing.ScopePath
 import scala.cli.commands.SpecificationLevel
 
 @DirectiveExamples("//> using python")
