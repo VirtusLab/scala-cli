12402ad1c9af9c27df7168b5a4191af49b61b519
Piotr Chabelski <ged.subfan@gmail.com>
2023-02-01 14:10:01 +0100

    Expand the help message for the `setup-ide` sub-command

--- modules/cli/src/main/scala/scala/cli/commands/setupide/SetupIdeOptions.scala
+++ modules/cli/src/main/scala/scala/cli/commands/setupide/SetupIdeOptions.scala
@@ -2,11 +2,28 @@ package scala.cli.commands.setupide
 
 import caseapp.*
 
-import scala.cli.commands.shared.{HasSharedOptions, SharedBspFileOptions, SharedOptions}
+import scala.cli.ScalaCli.{baseRunnerName, fullRunnerName}
+import scala.cli.commands.shared.{
+  HasSharedOptions,
+  HelpMessages,
+  SharedBspFileOptions,
+  SharedOptions
+}
 import scala.cli.commands.tags
 
 // format: off
-@HelpMessage("Generate a BSP file that you can import into your IDE")
+@HelpMessage(
+  s"""Generates a BSP file that you can import into your IDE.
+     |
+     |The `setup-ide` sub-command allows to pre-configure a $fullRunnerName project to import to an IDE with BSP support.
+     |It is also ran implicitly when `compile`, `run`, `shebang` or `test` sub-commands are called.
+     |
+     |The pre-configuration should be saved in a BSP json connection file under the path:
+     |    {project-root}/.bsp/$baseRunnerName.json
+     |
+     |${HelpMessages.commandConfigurations("setup-ide")}
+     |
+     |${HelpMessages.docsWebsiteReference}""".stripMargin)
 final case class SetupIdeOptions(
   @Recurse
     shared: SharedOptions = SharedOptions(),
