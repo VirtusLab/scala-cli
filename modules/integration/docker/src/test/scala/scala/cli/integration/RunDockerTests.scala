0f37522aeea4982a7d279b6a5f665986e067e1ea
Łukasz Wroński <46607934+lwronski@users.noreply.github.com>
2023-02-15 10:30:41 +0100

    Add clang to scala-cli docker image (#1846)

--- modules/integration/docker/src/test/scala/scala/cli/integration/RunDockerTests.scala
+++ modules/integration/docker/src/test/scala/scala/cli/integration/RunDockerTests.scala
@@ -10,6 +10,9 @@ class RunDockerTests extends munit.FunSuite {
   lazy val imageName = Option(System.getenv("SCALA_CLI_IMAGE")).getOrElse {
     sys.error("SCALA_CLI_IMAGE not set")
   }
+  lazy val termOpt = if (System.console() == null) Nil else Seq("-t")
+  lazy val ciOpt   = Option(System.getenv("CI")).map(v => Seq("-e", s"CI=$v")).getOrElse(Nil)
+  lazy val slimScalaCliImage = "scala-cli-slim"
 
   test("run simple app in in docker") {
     val fileName = "simple.sc"
@@ -21,8 +24,6 @@ class RunDockerTests extends munit.FunSuite {
            |""".stripMargin
     )
     inputs.fromRoot { root =>
-      val termOpt   = if (System.console() == null) Nil else Seq("-t")
-      val ciOpt     = Option(System.getenv("CI")).map(v => Seq("-e", s"CI=$v")).getOrElse(Nil)
       val rawOutput = new ByteArrayOutputStream
       val cmd = Seq[os.Shellable](
         // format: off
@@ -42,4 +43,22 @@ class RunDockerTests extends munit.FunSuite {
       expect(output.linesIterator.toVector.last == message)
     }
   }
+
+  if (!imageName.contains(slimScalaCliImage))
+    test("package simple app with native in docker") {
+      val fileName = "simple.sc"
+      val inputs = TestInputs(
+        os.rel / fileName -> """println("Hello")"""
+      )
+      inputs.fromRoot { root =>
+        val cmdPackage = Seq[os.Shellable](
+          // format: off
+          "docker", "run", "--rm", termOpt, "-v", s"$root:/data", "-w", "/data", ciOpt,
+          imageName, "package", "--native", fileName, "-o", "Hello"
+          // format: on
+        )
+        val procPackage = os.proc(cmdPackage).call(cwd = root, check = false)
+        expect(procPackage.exitCode == 0)
+      }
+    }
 }
