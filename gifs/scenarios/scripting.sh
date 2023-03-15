d1200f40c1a4d7e47c7f4756e3aed7194617c060
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-03-15 09:26:42 +0100

    Update libraries and outdated syntax in gifs

--- gifs/scenarios/scripting.sh
+++ gifs/scenarios/scripting.sh
@@ -11,7 +11,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 if [[ -z "${ASCIINEMA_REC}" ]]; then
   # Warm up scala-cli
   echo "println(1)" | scala-cli -
-  echo "//> using lib \"com.lihaoyi::os-lib::0.7.8\"" | scala-cli  -
+  echo "//> using dep \"com.lihaoyi::os-lib::0.9.1\"" | scala-cli  -
   # or do other preparation (e.g. create code)
 else
   . $SCRIPT_DIR/../demo-magic.sh
@@ -24,7 +24,7 @@ Hello World from file
 EOF
 
   cat <<EOF | updateFile script.sc
-//> using lib "com.lihaoyi::os-lib::0.7.8"
+//> using dep "com.lihaoyi::os-lib::0.9.1"
 
 val filePath = os.pwd / "file"
 val fileContent = os.read(filePath)
