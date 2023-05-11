c6c81ed90de32aad05f592aac71aa5fab55e7eab
Alex Archambault <alexandre.archambault@gmail.com>
2023-05-11 17:25:49 +0200

    Remove spurious character in error message

--- modules/core/src/main/scala/scala/build/errors/UnexpectedDirectiveError.scala
+++ modules/core/src/main/scala/scala/build/errors/UnexpectedDirectiveError.scala
@@ -1,4 +1,4 @@
 package scala.build.errors
 
 final class UnexpectedDirectiveError(val key: String)
-    extends BuildException(s"Unexpected directive: $key}")
+    extends BuildException(s"Unexpected directive: $key")
