fd67ed9e31736d53f495f1bbb0466422f7a4662b
Alex Archambault <alexandre.archambault@gmail.com>
2023-05-15 15:14:39 +0200

    Minor reformat
    Makes the diff of the next commit easier to read

--- modules/cli/src/main/scala/scala/cli/commands/util/JvmUtils.scala
+++ modules/cli/src/main/scala/scala/cli/commands/util/JvmUtils.scala
@@ -11,55 +11,56 @@ import scala.cli.commands.shared.{SharedJvmOptions, SharedOptions}
 import scala.util.Properties
 
 object JvmUtils {
-  def javaOptions(opts: SharedJvmOptions): Either[BuildException, JavaOptions] = either {
-    import opts._
+  def javaOptions(opts: SharedJvmOptions): Either[BuildException, JavaOptions] =
+    either {
+      import opts._
 
-    val (javacFilePlugins, javacPluginDeps) =
-      javacPlugin
-        .filter(_.trim.nonEmpty)
-        .partition { input =>
-          input.contains(File.separator) ||
-          (Properties.isWin && input.contains("/")) ||
-          input.count(_ == ':') < 2
-        }
-
-    val javaOptsSeq = {
-      val isDebug =
-        opts.sharedDebug.debug ||
-        opts.sharedDebug.debugMode.nonEmpty ||
-        opts.sharedDebug.debugPort.nonEmpty
-      if (isDebug) {
-        val server = value {
-          opts.sharedDebug.debugMode match {
-            case Some("attach") | Some("a") | None => Right("y")
-            case Some("listen") | Some("l")        => Right("n")
-            case Some(m)                           => Left(new UnrecognizedDebugModeError(m))
+      val (javacFilePlugins, javacPluginDeps) =
+        javacPlugin
+          .filter(_.trim.nonEmpty)
+          .partition { input =>
+            input.contains(File.separator) ||
+            (Properties.isWin && input.contains("/")) ||
+            input.count(_ == ':') < 2
           }
-        }
-        val port = opts.sharedDebug.debugPort.getOrElse("5005")
-        Seq(Positioned.none(
-          JavaOpt(s"-agentlib:jdwp=transport=dt_socket,server=$server,suspend=y,address=$port")
-        ))
-      }
-      else
-        Seq.empty
-    }
 
-    JavaOptions(
-      javaHomeOpt = javaHome.filter(_.nonEmpty).map(v =>
-        Positioned(Seq(Position.CommandLine("--java-home")), os.Path(v, Os.pwd))
-      ),
-      jvmIdOpt = jvm.filter(_.nonEmpty).map(Positioned.commandLine),
-      jvmIndexOpt = jvmIndex.filter(_.nonEmpty),
-      jvmIndexOs = jvmIndexOs.map(_.trim).filter(_.nonEmpty),
-      jvmIndexArch = jvmIndexArch.map(_.trim).filter(_.nonEmpty),
-      javaOpts = ShadowingSeq.from(javaOptsSeq),
-      javacPluginDependencies = SharedOptions.parseDependencies(
-        javacPluginDeps.map(Positioned.none(_)),
-        ignoreErrors = false
-      ),
-      javacPlugins = javacFilePlugins.map(s => Positioned.none(os.Path(s, Os.pwd))),
-      javacOptions = javacOption.map(Positioned.commandLine)
-    )
-  }
+      val javaOptsSeq = {
+        val isDebug =
+          opts.sharedDebug.debug ||
+          opts.sharedDebug.debugMode.nonEmpty ||
+          opts.sharedDebug.debugPort.nonEmpty
+        if (isDebug) {
+          val server = value {
+            opts.sharedDebug.debugMode match {
+              case Some("attach") | Some("a") | None => Right("y")
+              case Some("listen") | Some("l")        => Right("n")
+              case Some(m)                           => Left(new UnrecognizedDebugModeError(m))
+            }
+          }
+          val port = opts.sharedDebug.debugPort.getOrElse("5005")
+          Seq(Positioned.none(
+            JavaOpt(s"-agentlib:jdwp=transport=dt_socket,server=$server,suspend=y,address=$port")
+          ))
+        }
+        else
+          Seq.empty
+      }
+
+      JavaOptions(
+        javaHomeOpt = javaHome.filter(_.nonEmpty).map(v =>
+          Positioned(Seq(Position.CommandLine("--java-home")), os.Path(v, Os.pwd))
+        ),
+        jvmIdOpt = jvm.filter(_.nonEmpty).map(Positioned.commandLine),
+        jvmIndexOpt = jvmIndex.filter(_.nonEmpty),
+        jvmIndexOs = jvmIndexOs.map(_.trim).filter(_.nonEmpty),
+        jvmIndexArch = jvmIndexArch.map(_.trim).filter(_.nonEmpty),
+        javaOpts = ShadowingSeq.from(javaOptsSeq),
+        javacPluginDependencies = SharedOptions.parseDependencies(
+          javacPluginDeps.map(Positioned.none(_)),
+          ignoreErrors = false
+        ),
+        javacPlugins = javacFilePlugins.map(s => Positioned.none(os.Path(s, Os.pwd))),
+        javacOptions = javacOption.map(Positioned.commandLine)
+      )
+    }
 }
