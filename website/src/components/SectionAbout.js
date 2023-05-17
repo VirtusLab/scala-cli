50fe5eb82d15e7beca3cbb795130b63a24f5f5c2
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-05-17 13:02:23 +0200

    Add docs for bootstrapped standalone fat JAR

--- website/src/components/SectionAbout.js
+++ website/src/components/SectionAbout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default function SectionAbout(props){
   const id = props.title.toLowerCase().split(" ").join("-")
+  const colBigTitle = props.colBigTitle || 3
   const link = <a href={"#" + id} >&gt;_</a> 
   return <div className="section-about__wrapper row" id={id}>
         <div className="col col--1 big-title pre-title">{link}</div>
-        <div className="col col--3 big-title">
+        <div className={`col col--${colBigTitle} big-title`}>
             <span className="pre-title-mobile">{link}</span> {props.title}
         </div>
         <div className="col col--8 description">
