8a387f81ef2c12853fc3aa4360bc76ce26ef658c
Piotr Chabelski <ged.subfan@gmail.com>
2023-05-22 14:29:53 +0200

    NIT Add some scaladocs for the `toolkit` directive

--- modules/directives/src/main/scala/scala/build/preprocessing/directives/Toolkit.scala
+++ modules/directives/src/main/scala/scala/build/preprocessing/directives/Toolkit.scala
@@ -39,6 +39,13 @@ final case class Toolkit(
 }
 
 object Toolkit {
+
+  /** @param toolkitCoords
+    *   the toolkit coordinates
+    * @return
+    *   the `toolkit` and (potentially) `toolkit-test` dependencies with the appropriate build
+    *   requirements
+    */
   def resolveDependenciesWithRequirements(toolkitCoords: Positioned[String])
     : List[WithBuildRequirements[Positioned[DependencyLike[NameAttributes, NameAttributes]]]] =
     toolkitCoords match
@@ -65,16 +72,35 @@ object Toolkit {
               .withEmptyRequirements
           )
   val handler: DirectiveHandler[Toolkit] = DirectiveHandler.derive
+
+  /** Returns the `toolkit` (and potentially `toolkit-test`) dependency with the appropriate
+    * requirements.
+    *
+    * If [[defaultScope]] == None, it yields a List of up to 2 instances [[WithBuildRequirements]]
+    * of [[BuildOptions]], one with the `toolkit` dependency and no requirements, and one with the
+    * `toolkit-test` dependency and test scope requirements.
+    *
+    * If [[defaultScope]] == Some([[Scope.Test]]), then it yields a List with a single instance
+    * containing both dependencies and the test scope requirement.
+    *
+    * @param t
+    *   toolkit coordinates
+    * @param defaultScope
+    *   the scope requirement for the `toolkit` dependency
+    * @return
+    *   a list of [[Either]] [[BuildException]] [[WithBuildRequirements]] [[BuildOptions]]
+    *   containing the `toolkit` (and potentially `toolkit-test`) dependencies.
+    */
   private def buildOptionsWithScopeRequirement(
     t: Option[Positioned[String]],
     defaultScope: Option[Scope]
   ): List[Either[BuildException, WithBuildRequirements[BuildOptions]]] = t
     .toList
-    .flatMap(resolveDependenciesWithRequirements)
+    .flatMap(resolveDependenciesWithRequirements) // resolve dependencies
     .map { case WithBuildRequirements(requirements, positionedDep) =>
       positionedDep
         .withBuildRequirements {
-          if requirements.scope.isEmpty then
+          if requirements.scope.isEmpty then // if the scope is not set, set it to the default
             requirements.copy(scope = defaultScope.map(_.asScopeRequirement))
           else requirements
         }
@@ -90,7 +116,7 @@ object Toolkit {
     .toList
     .map { (scope: Option[Scope], boWithReqsList: List[WithBuildRequirements[BuildOptions]]) =>
       Right {
-        boWithReqsList.foldLeft {
+        boWithReqsList.foldLeft { // merge all the BuildOptions with the same scope requirement
           scope
             .map(s => BuildOptions.empty.withScopeRequirement(s))
             .getOrElse(BuildOptions.empty.withEmptyRequirements)
