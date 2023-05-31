a98d326d9f5a29b3f5557e914e3f2774833b8f67
Maciej Gajek <mgajek@virtuslab.com>
2023-05-31 14:28:04 +0200

    Add scaladoc for StdInConcurrentReader

--- modules/options/src/main/scala/scala/build/internal/StdInConcurrentReader.scala
+++ modules/options/src/main/scala/scala/build/internal/StdInConcurrentReader.scala
@@ -6,6 +6,19 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.io.StdIn
 
+/** Allows for reading StdIn concurrently, in a way that it can be interrupted. It was introduced in
+  * [[https://github.com/VirtusLab/scala-cli/pull/2168 #2168]] to fix input conflicts when watch and
+  * interactive modes are used together. <br>
+  *
+  * Two scenarios are possible when a new process uses [[waitforLine]] to read StdIn:
+  *   - if there is no ongoing reads taking place a future reading StdIn is started and the process
+  *     waits until there's a new input line or until it is interrupted
+  *   - if there is an ongoing read, the process waits for the result of the ongoing future or until
+  *     it is interrupted. <br>
+  *
+  * __Effectively, if used in parallel, the potential input is copied and distributed among the
+  * callers of [[waitForLine]]__
+  */
 object StdInConcurrentReader {
   private implicit val ec: ExecutionContext = ExecutionContext.global
   private val readLineFuture: AtomicReference[Future[Option[String]]] =
