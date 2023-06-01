f93b36ebf410c17fdd9723dff258dae7520cfb5c
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-06-01 15:00:25 +0200

    Add the .exe suffix to output provided by user for graalvm-native-image

--- modules/cli/src/main/scala/scala/cli/commands/package0/Package.scala
+++ modules/cli/src/main/scala/scala/cli/commands/package0/Package.scala
@@ -268,9 +268,14 @@ object Package extends ScalaCommand[PackageOptions] with BuildCommandHelpers {
         case _ if Properties.isWin                              => "app.bat"
         case _                                                  => "app"
       }
+      val output = outputOpt.map {
+        case o if packageType == PackageType.GraalVMNativeImage && Properties.isWin && !o.endsWith(".exe") =>
+          o + ".exe" // graalvm-native-image requires .exe extension on Windows
+        case o => o
+      }
 
       val packageOutput = build.options.notForBloopOptions.packageOptions.output
-      val dest = outputOpt.orElse(packageOutput)
+      val dest = output.orElse(packageOutput)
         .orElse {
           build.sources.defaultMainClass
             .map(n => n.drop(n.lastIndexOf('.') + 1))
