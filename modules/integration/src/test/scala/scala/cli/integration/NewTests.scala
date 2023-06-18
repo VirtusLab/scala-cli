11a602b6c5eef48f7f6b5abdaf10197e19f01a22
zetashift <rskaraya@gmail.com>
2023-06-18 15:37:04 +0200

    Improve integration tests in NewTests.scala

--- modules/integration/src/test/scala/scala/cli/integration/NewTests.scala
+++ modules/integration/src/test/scala/scala/cli/integration/NewTests.scala
@@ -2,6 +2,8 @@ package scala.cli.integration
 
 import com.eed3si9n.expecty.Expecty.expect
 
+import scala.cli.integration.TestUtil.removeAnsiColors
+  
 class NewTests extends ScalaCliSuite {
   override def group: ScalaCliSuite.TestGroup = ScalaCliSuite.TestGroup.First
 
@@ -21,15 +23,19 @@ class NewTests extends ScalaCliSuite {
 
   test("simple new template") {
     simpleTemplate.fromRoot { root =>
-      os.proc(TestUtil.cli, "new", simpleTemplateName).call(cwd = root)
+      os.proc(TestUtil.cli, "--power", "new", simpleTemplateName).call(cwd = root)
       val content = os.read(root / "HelloWorld.scala")
       expect(content == expectedTemplateContent)
     }
   }
 
-  test("error without template name") {
-    val output = os.proc(TestUtil.cli, "new")
-    expect(output == output)
+  test("error missing template argument") {
+    TestInputs.empty.fromRoot { root =>
+      val result = os.proc(TestUtil.cli, "--power", "new").call(cwd = root, check = false)
+      val lines = removeAnsiColors(result.err.text()).linesIterator.toVector
+      assert(result.exitCode == 1)
+      expect(lines.contains("Error: Missing argument <template>"))
+    }
   }
 
 }
