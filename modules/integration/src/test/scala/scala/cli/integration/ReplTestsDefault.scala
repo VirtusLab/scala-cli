35e6403692a15e00233d01d7a82681004bb0b680
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-07-24 13:52:40 +0200

    Add integration test

--- modules/integration/src/test/scala/scala/cli/integration/ReplTestsDefault.scala
+++ modules/integration/src/test/scala/scala/cli/integration/ReplTestsDefault.scala
@@ -2,6 +2,8 @@ package scala.cli.integration
 
 import com.eed3si9n.expecty.Expecty.expect
 
+import java.io.File
+
 import scala.util.Properties
 
 // format: off
@@ -60,4 +62,29 @@ class ReplTestsDefault extends ReplTestDefinitions(
       expect(asJarOutput == "hasDir=false")
     }
   }
+
+  if (TestUtil.isNativeCli)
+    test("not download java 17 when run repl without sources") {
+      TestInputs.empty.fromRoot { root =>
+        val java8Home =
+          os.Path(os.proc(TestUtil.cs, "java-home", "--jvm", "zulu:8").call().out.trim(), os.pwd)
+
+        val res =
+          os.proc(TestUtil.cli, "--power", "repl", TestUtil.extraOptions, "--", "-version").call(
+            cwd = root,
+            mergeErrIntoOut = true,
+            env = Map(
+              "JAVA_HOME"              -> java8Home.toString,
+              "COURSIER_ARCHIVE_CACHE" -> (root / "archive-cache").toString(),
+              "COURSIER_CACHE"         -> (root / "cache").toString(),
+              "PATH" -> ((java8Home / "bin").toString + File.pathSeparator + System.getenv("PATH"))
+            )
+          )
+
+        val output = res.out.trim().toLowerCase()
+
+        expect(!output.contains("jdk17"))
+        expect(!output.contains("jvm-index"))
+      }
+    }
 }
