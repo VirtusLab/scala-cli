675d4c5feb03ff85cc7e8cf859f1f39200babfad
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-09-22 13:05:21 +0200

    Add tests which check availability of scalafmt native launcher for default ScalaFmt version in scalafmt-native-image repo

--- modules/cli/src/test/scala/cli/tests/ScalafmtTest.scala
+++ modules/cli/src/test/scala/cli/tests/ScalafmtTest.scala
@@ -1,8 +1,13 @@
 package cli.tests
 import com.eed3si9n.expecty.Expecty.expect
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
+import scala.build.Ops.*
+import scala.build.internal.Constants
 import scala.build.tests.{TestInputs, TestLogger}
 import scala.cli.commands.fmt.{FmtOptions, FmtUtil}
+import scala.cli.commands.update.Update.Release
 
 class ScalafmtTests extends munit.FunSuite {
 
@@ -26,6 +31,39 @@ class ScalafmtTests extends munit.FunSuite {
       val readVersionAndDialect =
         FmtUtil.readVersionAndDialect(workspace = dirPath, FmtOptions(), TestLogger())
       expect(readVersionAndDialect == (None, None, None))
+    }
+  }
+
+  test("check native launcher availability for scalafmt") {
+    final case class Asset(name: String)
+    final case class Release(tag_name: String, assets: List[Asset])
+    lazy val releaseCodec: JsonValueCodec[Release] = JsonCodecMaker.make
+
+    val scalaFmtVersion = Constants.defaultScalafmtVersion
+    val url =
+      s"https://api.github.com/repos/virtuslab/scalafmt-native-image/releases/tags/v$scalaFmtVersion"
+
+    try {
+      val resp    = TestUtil.downloadFile(url).orThrow
+      val release = readFromArray(resp)(releaseCodec)
+      val assets  = release.assets.map(_.name)
+
+      val expectedAssets = Seq(
+        "scalafmt-x86_64-apple-darwin.gz",
+        "scalafmt-x86_64-pc-linux-mostly-static.gz",
+        "scalafmt-x86_64-pc-linux-static.gz",
+        "scalafmt-x86_64-pc-linux.gz",
+        "scalafmt-x86_64-pc-win32.zip"
+      )
+      // if this test fails, you should do a release of https://github.com/VirtusLab/scalafmt-native-image with the same version as scalafmt
+      expect(assets.containsSlice(expectedAssets))
+    }
+    catch {
+      case e: JsonReaderException => throw new Exception(s"Error reading $url", e)
+      case e: Throwable => throw new Exception(
+          s"Failed to check for the ScalaFmt native launcher assets: ${e.getMessage}",
+          e
+        )
     }
   }
 }
---
+++ modules/cli/src/test/scala/cli/tests/TestUtil.scala
@@ -0,0 +1,25 @@
+package cli.tests
+
+import coursier.cache.{ArtifactError, FileCache}
+import coursier.util.{Artifact, Task}
+
+import java.io.File
+
+import scala.concurrent.duration.{Duration, DurationInt}
+import scala.util.control.NonFatal
+
+object TestUtil {
+  def downloadFile(url: String): Either[ArtifactError, Array[Byte]] = {
+    val artifact = Artifact(url).withChanging(true)
+    val cache    = FileCache()
+
+    val file: Either[ArtifactError, File] = cache.logger.use {
+      try cache.withTtl(0.seconds).file(artifact).run.unsafeRun()(cache.ec)
+      catch {
+        case NonFatal(e) => throw new Exception(e)
+      }
+    }
+
+    file.map(f => os.read.bytes(os.Path(f, os.pwd)))
+  }
+}
