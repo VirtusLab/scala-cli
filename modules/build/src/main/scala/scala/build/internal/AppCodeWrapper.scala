d4409453d8db3510268dceda30aff84663d23785
Maciej Gajek <mgajek@virtuslab.com>
2023-11-13 11:09:14 +0100

    Add AppCodeWrapper

---
+++ modules/build/src/main/scala/scala/build/internal/AppCodeWrapper.scala
@@ -0,0 +1,31 @@
+package scala.build.internal
+
+case object AppCodeWrapper extends CodeWrapper {
+  def apply(
+    code: String,
+    pkgName: Seq[Name],
+    indexedWrapperName: Name,
+    extraCode: String,
+    scriptPath: String
+  ) = {
+    val wrapperObjectName  = indexedWrapperName.backticked
+
+    val packageDirective =
+      if (pkgName.isEmpty) "" else s"package ${AmmUtil.encodeScalaSourcePath(pkgName)}" + "\n"
+    val top = AmmUtil.normalizeNewlines(
+      s"""$packageDirective
+         |
+         |object $wrapperObjectName extends App {
+         |def scriptPath = \"\"\"$scriptPath\"\"\"
+         |""".stripMargin
+    )
+    val bottom = AmmUtil.normalizeNewlines(
+      s"""
+         |$extraCode
+         |}
+         |""".stripMargin
+    )
+
+    (top, bottom)
+  }
+}
