d075ac3a65215435c124a1a4f3cbcec0708998b0
Maciej Gajek <mgajek@virtuslab.com>
2023-12-20 17:52:31 +0100

    WIP

--- .github/scripts/pick-macos-xcode-sdk.sh
+++ .github/scripts/pick-macos-xcode-sdk.sh
@@ -1,3 +1,5 @@
-sudo xcode-select -s "/Applications/Xcode_13.1.0.app" || ( ls /Applications; exit 1 )
+#!/bin/bash
+
+sudo xcode-select -s "/Applications/Xcode_13.1.0.app" || ( ls /Applications && exit 1 )
 
 sudo rm -Rf /Library/Developer/CommandLineTools/SDKs/*
