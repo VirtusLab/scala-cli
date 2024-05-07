8e70efc3767398aff0675580e089c2a2b60c5b9a
Carlos de Paula <me@carlosedp.com>
2024-05-07 16:16:30 -0300

    Support arm64 arch for Mac on launcher script

--- scala-cli.sh
+++ scala-cli.sh
@@ -28,11 +28,20 @@ if [ "$(expr substr $(uname -s) 1 5 2>/dev/null)" == "Linux" ]; then
   fi
   CACHE_BASE="$HOME/.cache/coursier/v1"
 elif [ "$(uname)" == "Darwin" ]; then
-  SCALA_CLI_URL="https://github.com/$GH_ORG/$GH_NAME/releases/download/$TAG/scala-cli-x86_64-apple-darwin.gz"
-  CACHE_BASE="$HOME/Library/Caches/Coursier/v1"
+  arch=$(uname -m)
+  if [[ "$arch" == "x86_64" ]]; then
+    SCALA_CLI_URL="https://github.com/$GH_ORG/$GH_NAME/releases/download/$TAG/scala-cli-x86_64-apple-darwin.gz"
+    CACHE_BASE="$HOME/Library/Caches/Coursier/v1"
+  elif [[ "$arch" == "arm64" ]]; then
+    SCALA_CLI_URL="https://github.com/$GH_ORG/$GH_NAME/releases/download/$TAG/scala-cli-aarch64-apple-darwin.gz"
+    CACHE_BASE="$HOME/Library/Caches/Coursier/v1"
+  else
+    echoerr "scala-cli is not supported on $arch"
+    exit 2
+  fi
 else
-   echo "This standalone scala-cli launcher is supported only in Linux and macOS. If you are using Windows, please use the dedicated launcher scala-cli.bat"
-   exit 1
+  echo "This standalone scala-cli launcher is supported only in Linux and macOS. If you are using Windows, please use the dedicated launcher scala-cli.bat"
+  exit 1
 fi
 
 CACHE_DEST="$CACHE_BASE/$(echo "$SCALA_CLI_URL" | sed 's@://@/@')"
