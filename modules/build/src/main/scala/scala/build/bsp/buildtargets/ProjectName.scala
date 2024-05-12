01ceac9d2dfa758d517bff426617ca397164dcc1
Maciej Gajek <mgajek@virtuslab.com>
2024-05-12 18:20:29 +0200

    Fix ProjectName with test scope

--- modules/build/src/main/scala/scala/build/bsp/buildtargets/ProjectName.scala
+++ modules/build/src/main/scala/scala/build/bsp/buildtargets/ProjectName.scala
@@ -4,5 +4,5 @@ import scala.build.options.Scope
 
 final case class ProjectName(name: String) {
   def withScopeAppended(scope: Scope): ProjectName =
-    if scope == Scope.Main then this else copy(name = s"name-${scope.name.toLowerCase}")
+    if scope == Scope.Main then this else copy(name = s"$name-${scope.name.toLowerCase}")
 }
