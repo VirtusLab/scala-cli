fa6d8b60c5fdb3bc1272f456e3ee60865af8b1f0
Yadu Krishnan <yadavan88@gmail.com>
2024-07-14 18:22:10 +0200

    Separated scala specific tests into a separate trait to support Maven export

---
+++ modules/integration/src/test/scala/scala/cli/integration/ExportScalaOrientedBuildToolsTestDefinitions.scala
@@ -0,0 +1,36 @@
+package scala.cli.integration
+
+import com.eed3si9n.expecty.Expecty.expect
+
+import java.nio.charset.Charset
+
+/** This is a trait that defined test definitions for scala-oriented build tools like sbt and mill.
+  * The build tools like maven doesn't support some of the features like scalaJs, ScalaNative or
+  * compile-only dependencies.
+  */
+trait ExportScalaOrientedBuildToolsTestDefinitions {
+  _: ExportCommonTestDefinitions & ScalaCliSuite & TestScalaVersionArgs =>
+
+  def compileOnlyTest(): Unit = {
+    val userName = "John"
+    prepareTestInputs(
+      ExportTestProjects.compileOnlySource(actualScalaVersion, userName = userName)
+    ).fromRoot { root =>
+      exportCommand(".").call(cwd = root, stdout = os.Inherit)
+      val res = buildToolCommand(root, runMainArgs*)
+        .call(cwd = root / outputDir)
+      val output = res.out.trim(Charset.defaultCharset())
+      expect(output.contains(userName))
+      expect(!output.contains("jsoniter-scala-macros"))
+    }
+  }
+
+  if (runExportTests) {
+    test("compile-time only for jsoniter macros") {
+      compileOnlyTest()
+    }
+    test("Scala.js") {
+      simpleTest(ExportTestProjects.jsTest(actualScalaVersion))
+    }
+  }
+}
