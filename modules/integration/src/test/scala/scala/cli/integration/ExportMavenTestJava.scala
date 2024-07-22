efb1d4524f2543e7eaee52ef1cd08e6c87b259d2
Yadu Krishnan <yadavan88@gmail.com>
2024-07-22 22:16:38 +0200

    disabled running scala export tests in maven java export

--- modules/integration/src/test/scala/scala/cli/integration/ExportMavenTestJava.scala
+++ modules/integration/src/test/scala/scala/cli/integration/ExportMavenTestJava.scala
@@ -1,6 +1,8 @@
 package scala.cli.integration
 
 class ExportMavenTestJava extends ExportMavenTestDefinitions with Test3Lts with MavenJava {
+  // disable running scala tests in java maven export
+  override def runExportTests: Boolean = false
   test("pure java") {
     simpleTest(
       ExportTestProjects.pureJavaTest("ScalaCliJavaTest"),
