6c324f3ec5351b38ced43b38eb917ae0903a31be
Piotr Chabelski <ged.subfan@gmail.com>
2024-09-25 10:55:20 +0200

    Ensure `--version` passed to the default command together with `--offline` doesn't look for the latest version online

--- modules/integration/src/test/scala/scala/cli/integration/VersionTests.scala
+++ modules/integration/src/test/scala/scala/cli/integration/VersionTests.scala
@@ -5,17 +5,25 @@ import com.eed3si9n.expecty.Expecty.expect
 import scala.cli.integration.VersionTests.variants
 
 class VersionTests extends ScalaCliSuite {
-  test("version command") {
-    // tests if the format is correct instead of comparing to a version passed via Constants
-    // in order to catch errors in Mill configuration, too
-    val versionRegex = ".*\\d+[.]\\d+[.]\\d+.*".r
-    for (versionOption <- variants) {
-      val version = os.proc(TestUtil.cli, versionOption).call(check = false)
+
+  for (versionOption <- variants) {
+    test(versionOption) {
+      // tests if the format is correct instead of comparing to a version passed via Constants
+      // in order to catch errors in Mill configuration, too
+      val versionRegex = ".*\\d+[.]\\d+[.]\\d+.*".r
+      val version      = os.proc(TestUtil.cli, versionOption).call(check = false)
       assert(
         versionRegex.findFirstMatchIn(version.out.text()).isDefined,
         clues(version.exitCode, version.out.text(), version.err.text())
       )
       expect(version.exitCode == 0)
+    }
+
+    test(s"$versionOption --offline") {
+      TestInputs.empty.fromRoot { root =>
+        // TODO: --power should not be necessary here
+        os.proc(TestUtil.cli, versionOption, "--offline", "--power").call(cwd = root)
+      }
     }
   }
 
