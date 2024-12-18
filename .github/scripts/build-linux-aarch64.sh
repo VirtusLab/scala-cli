5591d9b5231610bc7c8cfb067ad3db7e02f85a79
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-12-18 12:00:36 +0100

    Update `coursier` to 2.1.22 (#3378)
    * Update coursier-jvm_2.13, ... to 2.1.22
    
    * Bump `coursier` to 2.1.22 in less obvious spots
    
    ---------
    
    Co-authored-by: Piotr Chabelski <ged.subfan@gmail.com>

--- .github/scripts/build-linux-aarch64.sh
+++ .github/scripts/build-linux-aarch64.sh
@@ -6,7 +6,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 mkdir -p artifacts
 mkdir -p utils
-cp "$(cs get https://github.com/VirtusLab/coursier-m1/releases/download/v2.1.21/cs-aarch64-pc-linux.gz --archive)" utils/cs
+cp "$(cs get https://github.com/VirtusLab/coursier-m1/releases/download/v2.1.22/cs-aarch64-pc-linux.gz --archive)" utils/cs
 chmod +x utils/cs
 
 cp "$DIR/build-linux-aarch64-from-docker.sh" utils/
