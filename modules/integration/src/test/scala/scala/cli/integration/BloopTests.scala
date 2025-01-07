2bac0d271e278b7979db190e50800ec5d75dea06
Piotr Chabelski <ged.subfan@gmail.com>
2025-01-07 16:39:02 +0100

    Make the `core` module respect custom repositories

--- modules/integration/src/test/scala/scala/cli/integration/BloopTests.scala
+++ modules/integration/src/test/scala/scala/cli/integration/BloopTests.scala
@@ -245,7 +245,7 @@ class BloopTests extends ScalaCliSuite {
   {
     val bloopSnapshotVersion = "2.0.6-51-38c118d4-SNAPSHOT"
     test(s"compilation works with a Bloop snapshot version: $bloopSnapshotVersion") {
-      val input                = "script.sc"
+      val input = "script.sc"
       TestInputs(os.rel / input -> """println("Hello")""").fromRoot { root =>
         os.proc(TestUtil.cli, "compile", input, "--bloop-version", bloopSnapshotVersion)
           .call(cwd = root)
