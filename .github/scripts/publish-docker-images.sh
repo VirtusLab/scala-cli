fa5f016d868dc26536f5317860914c0475b2951f
Piotr Chabelski <ged.subfan@gmail.com>
2025-06-04 17:06:05 +0200

    Fix standard docker images version

--- .github/scripts/publish-docker-images.sh
+++ .github/scripts/publish-docker-images.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
-SCALA_CLI_VERSION="$(./mill -i ci.publishVersion)"
+RAW_VERSION="$(./mill -i ci.publishVersion)"
+SCALA_CLI_VERSION="${RAW_VERSION##* }"
 
 docker tag scala-cli virtuslab/scala-cli:latest
 docker tag scala-cli virtuslab/scala-cli:"$SCALA_CLI_VERSION"
