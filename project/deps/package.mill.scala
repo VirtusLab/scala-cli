7b7c6e54fb393156fe4b531f74482fa73c6a5f73
Piotr Chabelski <ged.subfan@gmail.com>
2025-06-09 13:51:31 +0200

    Set Scala 3 Next RC announced version to 3.7.1-RC2

--- project/deps/package.mill.scala
+++ project/deps/package.mill.scala
@@ -17,7 +17,7 @@ object Scala {
   def scala3NextAnnounced =
     s"$scala3NextPrefix.0" // the newest/next version of Scala that's been announced
   def scala3NextRc        = "3.7.1-RC2"            // the latest RC version of Scala Next
-  def scala3NextRcAnnounced = "3.7.1-RC1" // the latest announced RC version of Scala Next
+  def scala3NextRcAnnounced = scala3NextRc // the latest announced RC version of Scala Next
 
   // The Scala version used to build the CLI itself.
   def defaultInternal = sys.props.get("scala.version.internal").getOrElse(scala3Lts)
