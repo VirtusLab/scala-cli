6ae99f2372356decebe108f99f49826456778ca4
Scala Steward <scala_steward@virtuslab.com>
2025-07-07 01:44:15 +0000

    Update sbt, scripted-plugin to 1.11.3

--- project/deps/package.mill.scala
+++ project/deps/package.mill.scala
@@ -134,7 +134,7 @@ object Deps {
     def javaSemanticdb                    = "0.10.0"
     def javaClassName                     = "0.1.7"
     def bloop                             = "2.0.10"
-    def sbtVersion                        = "1.11.2"
+    def sbtVersion                        = "1.11.3"
     def mavenVersion                      = "3.8.1"
     def mavenScalaCompilerPluginVersion   = "4.9.1"
     def mavenExecPluginVersion            = "3.3.0"
