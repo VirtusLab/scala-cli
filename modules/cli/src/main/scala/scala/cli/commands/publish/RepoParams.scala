7eafb3dbe242a6fc12973a8c4d646924ed7c1422
Piotr Chabelski <ged.subfan@gmail.com>
2025-07-08 14:35:40 +0200

    Switch `publish` command to point Sonatype publishing to Sonatype Central Portal OSSRH Staging API

--- modules/cli/src/main/scala/scala/cli/commands/publish/RepoParams.scala
+++ modules/cli/src/main/scala/scala/cli/commands/publish/RepoParams.scala
@@ -12,6 +12,7 @@ import java.util.concurrent.ScheduledExecutorService
 import scala.build.EitherCps.{either, value}
 import scala.build.Logger
 import scala.build.errors.BuildException
+import scala.build.internals.ConsoleUtils.ScalaCliConsole.warnPrefix
 import scala.cli.commands.util.ScalaCliSttpBackend
 
 final case class RepoParams(
@@ -49,7 +50,6 @@ final case class RepoParams(
 }
 
 object RepoParams {
-
   def apply(
     repo: String,
     vcsUrlOpt: Option[String],
@@ -67,24 +67,45 @@ object RepoParams {
       case "ivy2-local" =>
         RepoParams.ivy2Local(ivy2HomeOpt)
       case "sonatype" | "central" | "maven-central" | "mvn-central" =>
+        val ossrhStagingApiBase = "https://ossrh-staging-api.central.sonatype.com"
+        logger.message(s"Using Portal OSSRH Staging API: $ossrhStagingApiBase")
         RepoParams.centralRepo(
-          "https://oss.sonatype.org",
-          connectionTimeoutRetries,
-          connectionTimeoutSeconds,
-          stagingRepoRetries,
-          stagingRepoWaitTimeMilis,
-          es,
-          logger
+          base = ossrhStagingApiBase,
+          useLegacySnapshots = false,
+          connectionTimeoutRetries = connectionTimeoutRetries,
+          connectionTimeoutSeconds = connectionTimeoutSeconds,
+          stagingRepoRetries = stagingRepoRetries,
+          stagingRepoWaitTimeMilis = stagingRepoWaitTimeMilis,
+          es = es,
+          logger = logger
+        )
+      case "sonatype-legacy" | "central-legacy" | "maven-central-legacy" | "mvn-central-legacy" =>
+        val legacyBase = "https://oss.sonatype.org"
+        logger.message(s"$warnPrefix $legacyBase is EOL since 2025-06-30.")
+        logger.message(s"$warnPrefix $legacyBase publishing is expected to fail.")
+        RepoParams.centralRepo(
+          base = legacyBase,
+          useLegacySnapshots = true,
+          connectionTimeoutRetries = connectionTimeoutRetries,
+          connectionTimeoutSeconds = connectionTimeoutSeconds,
+          stagingRepoRetries = stagingRepoRetries,
+          stagingRepoWaitTimeMilis = stagingRepoWaitTimeMilis,
+          es = es,
+          logger = logger
         )
       case "sonatype-s01" | "central-s01" | "maven-central-s01" | "mvn-central-s01" =>
+        val s01SonatypeLegacyBase = "https://s01.oss.sonatype.org"
+        logger.message(s"$warnPrefix $s01SonatypeLegacyBase is EOL since 2025-06-30.")
+        logger.message(s"$warnPrefix it's expected publishing will fail.")
         RepoParams.centralRepo(
-          "https://s01.oss.sonatype.org",
-          connectionTimeoutRetries,
-          connectionTimeoutSeconds,
-          stagingRepoRetries,
-          stagingRepoWaitTimeMilis,
-          es,
-          logger
+          base = s01SonatypeLegacyBase,
+          useLegacySnapshots = true,
+          connectionTimeoutRetries = connectionTimeoutRetries,
+          connectionTimeoutSeconds = connectionTimeoutSeconds,
+          stagingRepoRetries = stagingRepoRetries,
+          stagingRepoWaitTimeMilis = stagingRepoWaitTimeMilis,
+          es = es,
+          logger = logger
         )
       case "github" =>
         value(RepoParams.gitHubRepo(vcsUrlOpt, workspace, logger))
@@ -103,77 +124,86 @@ object RepoParams {
         }
 
         RepoParams(
-          PublishRepository.Simple(repo0),
-          None,
-          Hooks.dummy,
-          isIvy2LocalLike,
-          true,
-          true,
-          true,
-          false,
-          false
+          repo = PublishRepository.Simple(repo0),
+          targetRepoOpt = None,
+          hooks = Hooks.dummy,
+          isIvy2LocalLike = isIvy2LocalLike,
+          defaultParallelUpload = true,
+          supportsSig = true,
+          acceptsChecksums = true,
+          shouldSign = false,
+          shouldAuthenticate = false
         )
     }
   }
 
   def centralRepo(
     base: String,
+    useLegacySnapshots: Boolean,
     connectionTimeoutRetries: Option[Int],
     connectionTimeoutSeconds: Option[Int],
     stagingRepoRetries: Option[Int],
     stagingRepoWaitTimeMilis: Option[Int],
     es: ScheduledExecutorService,
     logger: Logger
-  ) = {
-    val repo0   = PublishRepository.Sonatype(MavenRepository(base))
+  ): RepoParams = {
+    val repo0 = PublishRepository.Sonatype(
+      base = MavenRepository(base),
+      useLegacySnapshots = useLegacySnapshots
+    )
     val backend = ScalaCliSttpBackend.httpURLConnection(logger, connectionTimeoutSeconds)
     val api     = SonatypeApi(
-      backend,
-      base + "/service/local",
-      None,
-      logger.verbosity,
+      backend = backend,
+      base = base + "/service/local",
+      authentication = None,
+      verbosity = logger.verbosity,
       retryOnTimeout = connectionTimeoutRetries.getOrElse(3),
-      stagingRepoRetryParams = EmaRetryParams(
-        stagingRepoRetries.getOrElse(3),
-        stagingRepoWaitTimeMilis.getOrElse(10 * 1000),
-        2.0f
-      )
+      stagingRepoRetryParams =
+        EmaRetryParams(
+          attempts = stagingRepoRetries.getOrElse(3),
+          initialWaitDurationMs = stagingRepoWaitTimeMilis.getOrElse(10 * 1000),
+          factor = 2.0f
+        )
     )
     val hooks0 = Hooks.sonatype(
-      repo0,
-      api,
-      logger.compilerOutputStream, // meh
-      logger.verbosity,
+      repo = repo0,
+      api = api,
+      out = logger.compilerOutputStream, // meh
+      verbosity = logger.verbosity,
       batch = coursier.paths.Util.useAnsiOutput(), // FIXME Get via logger
-      es
+      es = es
     )
     RepoParams(
-      repo0,
-      Some("https://repo1.maven.org/maven2"),
-      hooks0,
-      false,
-      true,
-      true,
-      true,
-      true,
-      true
+      repo = repo0,
+      targetRepoOpt = Some("https://repo1.maven.org/maven2"),
+      hooks = hooks0,
+      isIvy2LocalLike = false,
+      defaultParallelUpload = true,
+      supportsSig = true,
+      acceptsChecksums = true,
+      shouldSign = true,
+      shouldAuthenticate = true
     )
   }
 
-  def gitHubRepoFor(org: String, name: String) =
+  def gitHubRepoFor(org: String, name: String): RepoParams =
     RepoParams(
-      PublishRepository.Simple(MavenRepository(s"https://maven.pkg.github.com/$org/$name")),
-      None,
-      Hooks.dummy,
-      false,
-      false,
-      false,
-      false,
-      false,
-      true
+      repo = PublishRepository.Simple(MavenRepository(s"https://maven.pkg.github.com/$org/$name")),
+      targetRepoOpt = None,
+      hooks = Hooks.dummy,
+      isIvy2LocalLike = false,
+      defaultParallelUpload = false,
+      supportsSig = false,
+      acceptsChecksums = false,
+      shouldSign = false,
+      shouldAuthenticate = true
     )
 
-  def gitHubRepo(vcsUrlOpt: Option[String], workspace: os.Path, logger: Logger) = either {
+  def gitHubRepo(
+    vcsUrlOpt: Option[String],
+    workspace: os.Path,
+    logger: Logger
+  ): Either[BuildException, RepoParams] = either {
     val orgNameFromVcsOpt = vcsUrlOpt.flatMap(GitRepo.maybeGhOrgName)
 
     val (org, name) = orgNameFromVcsOpt match {
@@ -184,7 +214,7 @@ object RepoParams {
     gitHubRepoFor(org, name)
   }
 
-  def ivy2Local(ivy2HomeOpt: Option[os.Path]) = {
+  def ivy2Local(ivy2HomeOpt: Option[os.Path]): RepoParams = {
     val home = ivy2HomeOpt
       .orElse(sys.props.get("ivy.home").map(prop => os.Path(prop)))
       .orElse(sys.props.get("user.home").map(prop => os.Path(prop) / ".ivy2"))
@@ -192,15 +222,15 @@ object RepoParams {
     val base = home / "local"
     // not really a Maven repo…
     RepoParams(
-      PublishRepository.Simple(MavenRepository(base.toNIO.toUri.toASCIIString)),
-      None,
-      Hooks.dummy,
-      true,
-      true,
-      true,
-      true,
-      false,
-      false
+      repo = PublishRepository.Simple(MavenRepository(base.toNIO.toUri.toASCIIString)),
+      targetRepoOpt = None,
+      hooks = Hooks.dummy,
+      isIvy2LocalLike = true,
+      defaultParallelUpload = true,
+      supportsSig = true,
+      acceptsChecksums = true,
+      shouldSign = false,
+      shouldAuthenticate = false
     )
   }
 
