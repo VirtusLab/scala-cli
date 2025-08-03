98746ce7c5087bbc0f4afefa0c0a77d33288bd02
Scala Steward <scala_steward@virtuslab.com>
2025-08-03 01:26:17 +0000

    Update scalafmt-cli_2.13, scalafmt-core to 3.9.9

--- project/deps/package.mill.scala
+++ project/deps/package.mill.scala
@@ -120,7 +120,7 @@ object Deps {
     def jsoniterScalaJava8                = "2.13.5.2"
     def jsoup                             = "1.21.1"
     def scalaMeta                         = "4.13.9"
-    def scalafmt                          = "3.9.8"
+    def scalafmt                          = "3.9.9"
     def scalaNative04                     = "0.4.17"
     def scalaNative05                     = "0.5.8"
     def scalaNative                       = scalaNative05
