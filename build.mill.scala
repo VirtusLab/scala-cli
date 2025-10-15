29dac881cf03f0cac77e8218a18f2570682848fb
Piotr Chabelski <ged.subfan@gmail.com>
2025-10-15 17:41:27 +0200

    Remove dead code handling Scala 2 modules in the build configuration

--- build.mill.scala
+++ build.mill.scala
@@ -17,7 +17,6 @@ import settings.{
   LocalRepo,
   PublishLocalNoFluff,
   ScalaCliCrossSbtModule,
-  ScalaCliScalafixLegacyModule,
   ScalaCliScalafixModule,
   jvmPropertiesFileName,
   localRepoResourcePath,
