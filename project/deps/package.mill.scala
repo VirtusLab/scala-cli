09125976fc7c6aed128b16cacf6d2f0eec1a824c
Piotr Chabelski <ged.subfan@gmail.com>
2025-12-03 09:47:20 +0100

    Bump `scalafmt` to 3.10.2 (was 3.10.1)

--- project/deps/package.mill.scala
+++ project/deps/package.mill.scala
@@ -134,7 +134,7 @@ object Deps {
     def jsoniterScala                     = "2.38.5"
     def jsoup                             = "1.21.2"
     def scalaMeta                         = "4.14.1"
-    def scalafmt                          = "3.10.1"
+    def scalafmt                          = "3.10.2"
     def scalaNative04                     = "0.4.17"
     def scalaNative05                     = "0.5.9"
     def scalaNative                       = scalaNative05
