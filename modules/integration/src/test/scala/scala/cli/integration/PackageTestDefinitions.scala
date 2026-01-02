7f689d00061565b9a535a6afba5ac9f222173337
krrish175-byte <krrishbiswas175@gmail.com>
2026-01-02 21:37:01 +0530

    Resolve merge conflict in PackageTestDefinitions imports

--- modules/integration/src/test/scala/scala/cli/integration/PackageTestDefinitions.scala
+++ modules/integration/src/test/scala/scala/cli/integration/PackageTestDefinitions.scala
@@ -8,7 +8,7 @@ import java.nio.file.Files
 import java.util
 import java.util.zip.ZipFile
 
-import scala.cli.integration.TestUtil.removeAnsiColors
+import scala.cli.integration.TestUtil.*
 import scala.concurrent.duration.DurationInt
 import scala.jdk.CollectionConverters.*
 import scala.util.{Properties, Using}
