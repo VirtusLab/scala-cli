6390db0e36d089bb6059b2c88289dd825d30587f
Piotr Chabelski <ged.subfan@gmail.com>
2026-05-20 12:49:48 +0200

    Add support for ```java snippets in Markdown inputs

--- modules/build/src/test/scala/scala/build/tests/PreprocessingTests.scala
+++ modules/build/src/test/scala/scala/build/tests/PreprocessingTests.scala
@@ -1,12 +1,32 @@
 package scala.build.tests
 
 import com.eed3si9n.expecty.Expecty.expect
+import coursier.cache.Cache.Fetch
+import coursier.cache.{ArchiveCache, ArtifactError, Cache}
+import coursier.util.{Artifact, EitherT, Task}
 
+import java.io.File
+
+import scala.build.Sources
 import scala.build.input.{MarkdownFile, ScalaCliInvokeData, Script, SourceScalaFile}
 import scala.build.options.SuppressWarningOptions
 import scala.build.preprocessing.{MarkdownPreprocessor, ScalaPreprocessor, ScriptPreprocessor}
+import scala.concurrent.ExecutionContext
 
 class PreprocessingTests extends TestUtil.ScalaCliBuildSuite {
+  private val markdownPreprocessor: MarkdownPreprocessor =
+    Sources.defaultPreprocessors(
+      ArchiveCache().withCache(
+        new Cache[Task] {
+          def fetch: Fetch[Task] = _ => sys.error("shouldn't be used")
+          def file(artifact: Artifact): EitherT[Task, ArtifactError, File] =
+            sys.error("shouldn't be used")
+          def ec: ExecutionContext = sys.error("shouldn't be used")
+        }
+      ),
+      javaClassNameVersionOpt = None,
+      javaCommand = () => sys.error("shouldn't be used")
+    ).collectFirst { case m: MarkdownPreprocessor => m }.get
   test("Report error if scala file not exists") {
     val logger    = TestLogger()
     val scalaFile = SourceScalaFile(os.temp.dir(), os.SubPath("NotExists.scala"))
@@ -45,7 +65,7 @@ class PreprocessingTests extends TestUtil.ScalaCliBuildSuite {
     val logger       = TestLogger()
     val markdownFile = MarkdownFile(os.temp.dir(), os.SubPath("NotExists.md"))
 
-    val res = MarkdownPreprocessor.preprocess(
+    val res = markdownPreprocessor.preprocess(
       markdownFile,
       logger,
       allowRestrictedFeatures = false,
--- modules/build/src/test/scala/scala/build/tests/markdown/MarkdownTestUtil.scala
+++ modules/build/src/test/scala/scala/build/tests/markdown/MarkdownTestUtil.scala
@@ -5,4 +5,6 @@ object MarkdownTestUtil {
   val RawScalaInfo: Seq[String]   = Seq("scala", "raw")
   val TestScalaInfo: Seq[String]  = Seq("scala", "test")
   val ResetScalaInfo: Seq[String] = Seq("scala", "reset")
+  val PlainJavaInfo: Seq[String]  = Seq("java")
+  val TestJavaInfo: Seq[String]   = Seq("java", "test")
 }
