542ad001cccd1487105d782e1ac08542361cb24d
Piotr Chabelski <ged.subfan@gmail.com>
2026-06-16 13:51:37 +0200

    Run tests with Bloop when the build server wasn't disabled

--- modules/integration/src/test/scala/scala/cli/integration/TestTests213.scala
+++ modules/integration/src/test/scala/scala/cli/integration/TestTests213.scala
@@ -3,33 +3,42 @@ package scala.cli.integration
 import com.eed3si9n.expecty.Expecty.expect
 
 import scala.cli.integration.TestUtil.StringOps
+import scala.util.Properties
 
-class TestTests213 extends TestTestDefinitions with Test213 {
-  test(s"run a simple test with Scala $actualScalaVersion (legacy)") {
-    val expectedMessage = "Hello, world!"
-    TestInputs(os.rel / "example.test.scala" ->
-      s"""//> using dep com.novocode:junit-interface:0.11
-         |import org.junit.Test
-         |
-         |class MyTests {
-         |  @Test
-         |  def foo(): Unit = {
-         |    assert(2 + 2 == 4)
-         |    println("$expectedMessage")
-         |  }
-         |}
-         |""".stripMargin).fromRoot { root =>
-      val expectedWarning =
-        s"Defaulting to a legacy test-runner module version: ${Constants.runnerScala2LegacyVersion}"
-      val res =
-        os.proc(TestUtil.cli, "test", ".", extraOptions)
-          .call(cwd = root, stderr = os.Pipe)
-      val out = res.out.trim()
-      expect(out.contains(expectedMessage))
-      val err = res.err.trim()
-      expect(err.contains(expectedWarning))
-      expect(err.countOccurrences(expectedWarning) == 1)
+trait TestTests213 { this: TestTestDefinitions & Test213 & TestBuildServer =>
+  if !usesBloop then
+    test(s"run a simple test with Scala $actualScalaVersion (legacy)") {
+      val expectedMessage = "Hello, world!"
+      TestInputs(os.rel / "example.test.scala" ->
+        s"""//> using dep com.novocode:junit-interface:0.11
+           |import org.junit.Test
+           |
+           |class MyTests {
+           |  @Test
+           |  def foo(): Unit = {
+           |    assert(2 + 2 == 4)
+           |    println("$expectedMessage")
+           |  }
+           |}
+           |""".stripMargin).fromRoot { root =>
+        val expectedWarning =
+          s"Defaulting to a legacy test-runner module version: ${Constants.runnerScala2LegacyVersion}"
+        val res =
+          os.proc(TestUtil.cli, "test", ".", extraOptions)
+            .call(cwd = root, stderr = os.Pipe)
+        val out = res.out.trim()
+        expect(out.contains(expectedMessage))
+        val err = res.err.trim()
+        expect(err.contains(expectedWarning))
+        expect(err.countOccurrences(expectedWarning) == 1)
+      }
     }
-  }
+}
 
+class TestTests213WithBloop
+    extends TestTestDefinitions with Test213 with TestWithBloop with TestTests213
+
+class TestTests213WithoutBloop
+    extends TestTestDefinitions with Test213 with TestWithoutBloop with TestTests213 {
+  override def munitIgnore: Boolean = super.munitIgnore || Properties.isWin
 }
