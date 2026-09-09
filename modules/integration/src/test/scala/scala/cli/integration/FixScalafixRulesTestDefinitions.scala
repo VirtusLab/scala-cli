d827769091758bdd9de4b05107e57ad5ac2d9ff8
Piotr Chabelski <ged.subfan@gmail.com>
2026-09-09 11:20:42 +0200

    Temporarily disable `fix` tests using `ExplicitResultTypes` rule for Scala 3.10+ (depends on scalacenter/scalafix#2512)

--- modules/integration/src/test/scala/scala/cli/integration/FixScalafixRulesTestDefinitions.scala
+++ modules/integration/src/test/scala/scala/cli/integration/FixScalafixRulesTestDefinitions.scala
@@ -312,9 +312,9 @@ trait FixScalafixRulesTestDefinitions {
       Seq("--semanticdb=false") -> false
     )
     semanticDbOptionsDescription =
-      if (semanticDbOptions.nonEmpty) s" (${semanticDbOptions.mkString(" ")})"
-      else ""
-    verb = if (expectedSuccess) "run" else "fail"
+      if semanticDbOptions.nonEmpty then s" (${semanticDbOptions.mkString(" ")})" else ""
+    verb = if expectedSuccess then "run" else "fail"
+    if !isScala310OrNewer || !expectedSuccess // TODO re-enable when fixed for 3.10+
     if !Properties.isWin || expectedSuccess
   }
     test(
