4bebb5f6ff1cc231555ea25470c1d7c7d404ed51
Piotr Chabelski <ged.subfan@gmail.com>
2026-09-11 13:45:58 +0200

    source migration: -source:3.7-migration

--- modules/integration/src/test/scala/scala/cli/integration/RunTests3Lts.scala
+++ modules/integration/src/test/scala/scala/cli/integration/RunTests3Lts.scala
@@ -37,7 +37,7 @@ class RunTests3Lts extends RunTestDefinitions with RunLtsTagTestDefinitions with
         ) { (proc, timeout, ec) =>
           val output1 = TestUtil.readLine(proc.stdout, ec, timeout)
           expect(output1 == expectedMessage1)
-          proc.printStderrUntilRerun(timeout)(ec)
+          proc.printStderrUntilRerun(timeout)(using ec)
           os.write.over(root / inputPath, code(expectedMessage2))
           val output2 = TestUtil.readLine(proc.stdout, ec, timeout)
           expect(output2 == expectedMessage2)
