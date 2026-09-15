f8f8ce8900f90b9f3489bb7c3ddb46a7d846a0c1
Piotr Chabelski <ged.subfan@gmail.com>
2026-09-15 13:27:49 +0200

    Bump Mill to 1.1.9 (was 1.1.7) (#4472)

--- mill.bat
+++ mill.bat
@@ -2,7 +2,7 @@
 
 setlocal enabledelayedexpansion
 
-if [!DEFAULT_MILL_VERSION!]==[] ( set "DEFAULT_MILL_VERSION=1.1.7" )
+if [!DEFAULT_MILL_VERSION!]==[] ( set "DEFAULT_MILL_VERSION=1.1.9" )
 
 if [!MILL_GITHUB_RELEASE_CDN!]==[] ( set "MILL_GITHUB_RELEASE_CDN=" )
 
