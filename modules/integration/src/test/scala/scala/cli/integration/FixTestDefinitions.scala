b2c63a9ffa38404bb0e4c355f7b619b2f2881c11
Jakub Warchoł <warcholljakub@gmail.com>
2026-09-23 19:17:33 +0200

    fix: retain directive aliases in `fix`

--- modules/integration/src/test/scala/scala/cli/integration/FixTestDefinitions.scala
+++ modules/integration/src/test/scala/scala/cli/integration/FixTestDefinitions.scala
@@ -25,7 +25,7 @@ abstract class FixTestDefinitions
     val directive1           = "//> using dep com.lihaoyi::os-lib:0.11.3"
     val directive2           = "//> using dep com.lihaoyi::pprint:0.9.0"
     val mergedDirective1And2 =
-      "using dependency com.lihaoyi::os-lib:0.11.3 com.lihaoyi::pprint:0.9.0"
+      "using dep com.lihaoyi::os-lib:0.11.3 com.lihaoyi::pprint:0.9.0"
     val directive3 =
       if (actualScalaVersion.startsWith("2")) "//> using options -Xlint:unused"
       else "//> using options -Wunused:all"
